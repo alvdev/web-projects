47da5024d1500a610e59a76e9b22f7a6fe0766a9
alvdev <soy@alv.dev>
2021-12-19 14:47:37 +0100

    Added tabs funcionality

--- alfa.host/user/themes/alfa/js/marketing-services.js
+++ alfa.host/user/themes/alfa/js/marketing-services.js
@@ -1,1 +1,11 @@
-console.log("this is marketing services block");
+const tabs = document.querySelectorAll('.services ul#tabs li a');
+const tabsContent = document.querySelectorAll('.services div[id*=tabcontent]');
+
+const activeTabContentId = tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const tabId = tab.getAttribute('id');
+    return tabId + 'content';
+  });
+});
+
+console.log(activeTabContentId);
