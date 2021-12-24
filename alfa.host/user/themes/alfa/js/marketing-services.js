42373679c141148abb83016404f6cff74b5394c2
alvdev <soy@alv.dev>
2021-12-24 18:24:43 +0100

    Fixed on click active tab

--- alfa.host/user/themes/alfa/js/marketing-services.js
+++ alfa.host/user/themes/alfa/js/marketing-services.js
@@ -1,11 +1,33 @@
 const tabs = document.querySelectorAll('.services ul#tabs li a');
-const tabsContent = document.querySelectorAll('.services div[id*=tabcontent]');
+const tabsContent = document.querySelectorAll('.services [id*=tabcontent]');
 
-const activeTabContentId = tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const tabId = tab.getAttribute('id');
-    return tabId + 'content';
+const activeTab = tabs.forEach(tab => {
+  tab.addEventListener('click', (e, tabContentId) => {
+    if (!tab.classList.contains('active')) {
+      tabs.forEach(tab => {
+        tab.classList.remove('active');
+      });
+      tab.classList.add('active');
+    }
+
+    tabsContent.forEach(tabContent => {
+      console.log(tab.getAttribute('id'));
+      if (!tabContent.getAttribute('id').includes(tab.getAttribute('id'))) {
+        console.log('Working');
+        tabsContent.forEach(tabContent => {
+          tabContent.classList.add('hidden');
+        });
+        tabContent.classList.remove('hidden');
+      }
+    });
   });
 });
 
-console.log(activeTabContentId);
+// console.log(activeTab);
+
+// const activeTabContent = tabs.forEach(tab => {
+//   const tabActiveId = tab.getAttribute('id');
+//   console.log(tabActiveId);
+// });
+
+// console.log(activeTabContentId);
