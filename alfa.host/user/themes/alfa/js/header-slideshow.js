51aaed201d2dbffb95011d8a7ce47e339910f631
alvdev <soy@alv.dev>
2022-02-01 17:42:59 +0100

    Removed unnecessary object properties

--- alfa.host/user/themes/alfa/js/header-slideshow.js
+++ alfa.host/user/themes/alfa/js/header-slideshow.js
@@ -1,15 +1,4 @@
 new Glide('.glide-header', {
   type: 'carousel',
   animationDuration: 1000,
-  perView: 1,
-  gap: 0,
-  peek: 0,
-  breakpoints: {
-    800: {
-      peek: 0,
-    },
-    400: {
-      peek: 0,
-    },
-  },
-}).mount;
+}).mount();
