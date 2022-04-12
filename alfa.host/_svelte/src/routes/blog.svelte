d20f86ba4cbf2a4e0819e1b8fe621e49fbcb4cb9
alvdev <soy@alv.dev>
2022-04-13 00:03:37 +0200

    Added Svelte scaffolding from first project

---
+++ alfa.host/_svelte/src/routes/blog.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Footer from '/src/components/sections/Footer.svelte';
+  import Header from '/src/components/sections/Header.svelte';
+</script>
+
+<svelte:head>
+  <title>Alfa Host Blog</title>
+</svelte:head>
+
+<Header />
+
+<Footer />
