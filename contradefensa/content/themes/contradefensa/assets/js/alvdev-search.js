2a5626b42b8d2bf97ae013cddb20ffdf9059c8b0
alvdev <soy@alv.dev>
2022-06-10 07:36:39 +0200

    Added collapsible input search field

--- contradefensa/content/themes/contradefensa/assets/js/alvdev-search.js
+++ contradefensa/content/themes/contradefensa/assets/js/alvdev-search.js
@@ -5,8 +5,26 @@ new GhostFinder({
   excerpt_length: 150,
   singleResultTemplate: `
       <li>
-          <a href="##url">##title</a>
-          <p>##excerpt</p>
+        <a href="##url">##title</a>
+        <p>##excerpt</p>
       </li>
     `,
+});
+
+const searchBtn = document.querySelector('#search-btn');
+const searchInput = document.querySelector('#search-input');
+const menu = document.querySelector('.gh-head-menu');
+const searchResult = document.querySelector('#search-result');
+
+searchBtn.addEventListener('click', e => {
+  e.preventDefault();
+  searchInput.classList.add('opened');
+  menu.style.display = 'none';
+  searchInput.focus();
+});
+
+searchInput.addEventListener('focusout', e => {
+  searchInput.classList.remove('opened');
+  menu.style.display = 'flex';
+  searchResult.innerHTML = '';
 });
