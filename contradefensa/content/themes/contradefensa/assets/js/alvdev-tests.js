b14a706c7602f2243a156f94f2d99497ade702ce
alvdev <soy@alv.dev>
2022-06-18 19:33:23 +0200

    Fetch questions from external API

--- contradefensa/content/themes/contradefensa/assets/js/alvdev-tests.js
+++ contradefensa/content/themes/contradefensa/assets/js/alvdev-tests.js
@@ -1,3 +1,12 @@
+async function fetchQuestions() {
+  const response = await fetch('http://localhost:8055/items/questions');
+  const data = await response.json();
+  const questions = data['data'];
+  availableQuestions = [...questions];
+}
+
+// End testing
+
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 
@@ -7,50 +16,53 @@ let questionCounter = 0;
 let score = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: 'This is the first question',
-    choice1: 'Choice 1 to first question',
-    choice2: 'Choice 2 to first question',
-    choice3: 'Choice 3 to first question',
-    choice4: 'Choice 4 to first question',
-    answer: 3,
-  },
-  {
-    question: 'This is the second question',
-    choice1: 'Choice 1 to second question',
-    choice2: 'Choice 2 to second question',
-    choice3: 'Choice 3 to second question',
-    choice4: 'Choice 4 to second question',
-    answer: 4,
-  },
-  {
-    question: 'This is the third question',
-    choice1: 'Choice 1 to third question',
-    choice2: 'Choice 2 to third question',
-    choice3: 'Choice 3 to third question',
-    choice4: 'Choice 4 to third question',
-    answer: 2,
-  },
-  {
-    question: 'This is the fourth question',
-    choice1: 'Choice 1 to fourth question',
-    choice2: 'Choice 2 to fourth question',
-    choice3: 'Choice 3 to fourth question',
-    choice4: 'Choice 4 to fourth question',
-    answer: 1,
-  },
-];
+// let questions = [
+//   {
+//     id: 1,
+//     question: 'This is the first question',
+//     choice1: 'Choice 1 to first question',
+//     choice2: 'Choice 2 to first question',
+//     choice3: 'Choice 3 to first question',
+//     choice4: 'Choice 4 to first question',
+//     answer: 3,
+//   },
+//   {
+//     id: 2,
+//     question: 'This is the second question',
+//     choice1: 'Choice 1 to second question',
+//     choice2: 'Choice 2 to second question',
+//     choice3: 'Choice 3 to second question',
+//     choice4: 'Choice 4 to second question',
+//     answer: 4,
+//   },
+//   {
+//     id: 3,
+//     question: 'This is the third question',
+//     choice1: 'Choice 1 to third question',
+//     choice2: 'Choice 2 to third question',
+//     choice3: 'Choice 3 to third question',
+//     choice4: 'Choice 4 to third question',
+//     answer: 2,
+//   },
+//   {
+//     id: 4,
+//     question: 'This is the fourth question',
+//     choice1: 'Choice 1 to fourth question',
+//     choice2: 'Choice 2 to fourth question',
+//     choice3: 'Choice 3 to fourth question',
+//     choice4: 'Choice 4 to fourth question',
+//     answer: 1,
+//   },
+// ];
 
 // Constants
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
-  availableQuestions = [...questions];
-  console.log(availableQuestions);
+  availableQuestions = fetchQuestions();
   getNewQuestion();
 };
 
