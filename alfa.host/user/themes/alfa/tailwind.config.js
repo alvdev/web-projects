81035da3ef34793d3d891a728baee4c32d5f2b5d
Alvaro Devesa <alvdev@outlook.com>
2022-11-01 20:15:16 +0100

    Add custom filter drop shadows

--- alfa.host/user/themes/alfa/tailwind.config.js
+++ alfa.host/user/themes/alfa/tailwind.config.js
@@ -24,9 +24,21 @@ module.exports = {
         pxx: '2px',
       },
     },
+    dropShadow: {
+      icon: '4px -4px 4px rgba(15, 23, 42, 0.20)',
+      xs: '0 2px 2px rgba(15, 23, 42, 0.20)',
+      lg: '0 6px 6px rgba(15, 23, 42, 0.20)',
+      xl: '0 10px 10px rgba(15, 23, 42, 0.20)',
+      '2xl': '0 20px 20px rgba(15, 23, 42, 0.20)',
+      '3xl': '0 35px 35px rgba(15, 23, 42, 0.20)',
+      '4xl': [
+        '0 35px 35px rgba(15, 23, 42, 0.25)',
+        '0 45px 65px rgba(15, 23, 42, 0.15)',
+      ],
+    },
     colors: {
       white: colors.white,
-      gray: colors.blueGray,
+      gray: colors.slate,
       black: colors.black,
       blue: colors.blue,
       sky: colors.sky,
