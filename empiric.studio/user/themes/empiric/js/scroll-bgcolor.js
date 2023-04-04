4714a5a55f56e46c2a3eedb85866778216edb30f
alv.dev <soy@alv.dev>
2023-04-04 08:41:33 +0200

    feat: add dataset color to dark sections instead dark class

--- empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
+++ empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
@@ -2,9 +2,11 @@ const darkElems = document.querySelectorAll(".dark");
 
 window.addEventListener("scroll", () => {
   for (let i of darkElems) {
+    const classes = i.dataset.color.split(" ");
+    console.log(classes);
     window.scrollY >= i.offsetTop - window.innerHeight / 3 &&
     window.scrollY <= i.offsetTop + i.clientHeight - window.innerHeight / 3
-      ? document.body.classList.add("bg-gray-900")
-      : document.body.classList.remove("bg-gray-900");
+      ? document.body.classList.add(...classes)
+      : document.body.classList.remove(...classes);
   }
 });
