96de0633e38cd1245ebed5705afff15f58959747
alv.dev <soy@alv.dev>
2023-04-10 19:07:11 +0200

    style: prettier formatting

--- empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
+++ empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
@@ -1,20 +1,20 @@
-const sections = document.querySelectorAll("[data-color]");
-const arrow = document.querySelector("#totop svg path");
+const sections = document.querySelectorAll('[data-color]');
+const arrow = document.querySelector('#totop svg path');
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   for (let section of sections) {
-    const classes = section.dataset.color.split(" ");
+    const classes = section.dataset.color.split(' ');
     if (
       window.scrollY >= section.offsetTop - window.innerHeight / 3 &&
       window.scrollY <=
         section.offsetTop + section.clientHeight - window.innerHeight / 3
     ) {
       document.body.classList.add(...classes);
-      arrow.style.fill = "white";
+      arrow.style.fill = 'white';
       break;
     } else {
       document.body.classList.remove(...classes);
-      arrow.style.fill = "black";
+      arrow.style.fill = 'black';
     }
   }
 });
