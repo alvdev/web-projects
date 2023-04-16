9e4d66196a765173cb9c243cfe78112e9493f81c
alv.dev <soy@alv.dev>
2023-04-16 22:10:04 +0200

    fix: logo color/transition over dark sections

--- empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
+++ empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
@@ -1,14 +1,21 @@
 const sections = document.querySelectorAll('[data-color]');
 const arrow = document.querySelector('#totop svg path');
+const empiric = document.querySelectorAll('aside .logo path.c');
+const triangle = document.querySelector('aside .logo polygon.a');
+console.log(triangle);
 
 const sectionObserver = function (entries) {
   entries.forEach(entry => {
     const classes = entry.target.dataset.color.split(' ');
     if (entry.isIntersecting) {
       document.body.classList.add(...classes);
+      triangle.style.fill = '#7e22ce';
+      empiric.forEach(c => (c.style.fill = 'white'));
       arrow.style.fill = 'white';
     } else {
       document.body.classList.remove(...classes);
+      triangle.style.fill = 'gold';
+      empiric.forEach(c => (c.style.fill = '#101010'));
       arrow.style.fill = 'black';
     }
   });
