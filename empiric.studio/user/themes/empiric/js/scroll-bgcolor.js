32a316d487950c696cc8a65ac74779c6bc628d41
alv.dev <soy@alv.dev>
2023-09-09 13:21:37 +0200

    fix: left size responsive backgrounds

--- empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
+++ empiric.studio/user/themes/empiric/js/scroll-bgcolor.js
@@ -2,6 +2,7 @@ const sections = document.querySelectorAll('[data-color]');
 const arrow = document.querySelector('#totop svg path');
 const empiric = document.querySelectorAll('aside .logo path.c');
 const triangle = document.querySelector('aside .logo polygon.a');
+const aside = document.querySelector('aside');
 
 const sectionObserver = function (entries) {
   entries.forEach(entry => {
@@ -11,11 +12,27 @@ const sectionObserver = function (entries) {
       triangle.style.fill = '#7e22ce';
       empiric.forEach(c => (c.style.fill = 'white'));
       arrow.style.fill = 'white';
+      aside.classList.add(
+        'bg-gray-900',
+        'shadow-[0_0_30px_30px_rgb(24,24,27)]'
+      );
+      aside.classList.remove(
+        'bg-white',
+        'shadow-[0_0_30px_30px_rgb(255,255,255)]'
+      );
     } else {
       document.body.classList.remove(...classes);
       triangle.style.fill = 'gold';
       empiric.forEach(c => (c.style.fill = '#101010'));
       arrow.style.fill = 'black';
+      aside.classList.add(
+        'bg-white',
+        'shadow-[0_0_30px_30px_rgb(255,255,255)]'
+      );
+      aside.classList.remove(
+        'bg-gray-900',
+        'shadow-[0_0_30px_30px_rgb(24,24,27)]'
+      );
     }
   });
 };
