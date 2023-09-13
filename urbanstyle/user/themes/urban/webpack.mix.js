1abb6d9af5f25864fa31c6dfadf84c0d085ebe2d
alv.dev <soy@alv.dev>
2023-09-13 20:16:46 +0200

    compile with mix for production

--- urbanstyle/user/themes/urban/webpack.mix.js
+++ urbanstyle/user/themes/urban/webpack.mix.js
@@ -2,16 +2,24 @@ let mix = require('laravel-mix');
 require('mix-tailwindcss');
 
 const extension = mix.inProduction() ? '.min' : '';
+const partytown = require('./node_modules/@builder.io/partytown/utils');
 
-mix.js('js/main.js', `js/main${extension}.js`)
-    .css('css/site.css', `css/site${extension}.css`)
-    .tailwind()
-    .setPublicPath('dist')
-    .version()
-    // .browserSync({
-    //     proxy: 'http://domain.test',
-    //     files: [
-    //         './dist/mix-manifest.json',
-    //         '../../pages/**/*.md'
-    //     ]
-    // })
+mix
+  .js('js/main.js', `js/main${extension}.js`)
+  .css('css/site.css', `css/site${extension}.css`)
+  .tailwind()
+  .setPublicPath('dist')
+  .version()
+  .copy('icons', 'dist/icons')
+  .copy('images', 'dist/images')
+  .copy(partytown.libDirPath(), 'dist/~partytown')
+  .options({
+    processCssUrls: false,
+  });
+// .browserSync({
+//     proxy: 'http://domain.test',
+//     files: [
+//         './dist/mix-manifest.json',
+//         '../../pages/**/*.md'
+//     ]
+// })
