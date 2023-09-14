1f969cfc016b06c2f291cac1a14e82e217df18e6
alv.dev <soy@alv.dev>
2023-09-14 23:00:08 +0200

    feat: add form class variables to urban settings

--- urbanstyle/user/themes/urban/urban.php
+++ urbanstyle/user/themes/urban/urban.php
@@ -1,9 +1,31 @@
 <?php
+
 namespace Grav\Theme;
 
 use Grav\Common\Theme;
 
 class Urban extends Theme
 {
-    // Access plugin events in this class
+    public function onTwigInitialized()
+    {
+        $twig = $this->grav['twig'];
+
+        $form_class_variables = [
+            // 'form_outer_classes' => 'form-horizontal',
+            'form_button_outer_classes' => 'flex gap-4 mt-12',
+            // 'form_button_classes' => 'btn',
+            // 'form_errors_classes' => '',
+            // 'form_field_outer_classes' => 'form-group',
+            // 'form_field_outer_label_classes' => 'form-label-wrapper',
+            // 'form_field_label_classes' => 'form-label',
+            // 'form_field_outer_data_classes' => 'col-9',
+            // 'form_field_input_classes' => 'form-input',
+            // 'form_field_textarea_classes' => '',
+            // 'form_field_select_classes' => 'form-select',
+            // 'form_field_radio_classes' => 'form-radio',
+            // 'form_field_checkbox_classes' => 'form-checkbox',
+        ];
+
+        $twig->twig_vars = array_merge($twig->twig_vars, $form_class_variables);
+    }
 }
