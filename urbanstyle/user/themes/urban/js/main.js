478c9b39cb1dccc931b620f8950b4e65964e41ff
alv.dev <soy@alv.dev>
2024-03-15 17:13:09 +0100

    ➕ add alpine ajax/intersect plugins

--- urbanstyle/user/themes/urban/js/main.js
+++ urbanstyle/user/themes/urban/js/main.js
@@ -1,7 +1,12 @@
 import Alpine from '../node_modules/alpinejs';
-
 import collapse from '../node_modules/@alpinejs/collapse';
-Alpine.plugin(collapse);
+import intersect from '../node_modules/@alpinejs/intersect';
+import ajax from '../node_modules/@imacrayon/alpine-ajax';
 
 window.Alpine = Alpine;
+
+Alpine.plugin(collapse);
+Alpine.plugin(intersect);
+Alpine.plugin(ajax);
+
 Alpine.start();
