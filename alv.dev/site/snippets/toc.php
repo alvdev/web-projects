6bb9e50c9751a5cb56e13a10f9368175e01ddad6
alv.dev <soy@alv.dev>
2024-08-09 17:33:03 +0200

    add alternative article's TOC

--- alv.dev/site/snippets/toc.php
+++ alv.dev/site/snippets/toc.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * Valid only for TOC pages with blocks
+ * TOC is working with JS approach for now (blocks/toc.php)
+ */
+?>
+
 <?php if ($headlines->count() >= 3) : ?>
     <nav class="toc">
         <h2>Table of Contents</h2>
