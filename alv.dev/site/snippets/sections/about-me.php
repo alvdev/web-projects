755e7cde62d546e0f2721a3a2ab1e960484b84fa
alv.dev <soy@alv.dev>
2024-08-16 04:26:27 +0200

    (about me): add sticky scroll

--- alv.dev/site/snippets/sections/about-me.php
+++ alv.dev/site/snippets/sections/about-me.php
@@ -1,23 +1,40 @@
-<section class="py-28 bg-amber-100 bg-opacity-70 border-t-2 border-white id="aboutme">
+<section id="aboutme" class="py-28 bg-amber-100 bg-opacity-70 border-t-2 border-white">
     <div class="container flex gap-28">
-        <hgroup class="w-2/5 flex flex-wrap flex-col gap-y-2 md:text-center xl:text-left mb-10">
-            <div class="text-amber-600 text-xl">Sobre mí</div>
-            <h2 class="font-semibold text-4xl lg:text-5xl xl:text-6xl">
-                La parte menos importante
-            </h2>
-        </hgroup>
+        <div class="w-2/5">
+            <hgroup class="sticky top-8 flex flex-wrap flex-col gap-y-2 md:text-center xl:text-left mb-10">
+                <div class="text-amber-600 text-xl">
+                    Sobre mí
+                </div>
+                <h2 class="font-semibold text-4xl lg:text-5xl xl:text-6xl">
+                    La parte menos importante
+                </h2>
+            </hgroup>
+        </div>
 
         <div class="w-3/5">
             <div id="aboutme" class="tab-content relative active">
                 <div class="grid grid-cols-1">
                     <h3 class="text-black-800 text-2xl font-bold mb-6">Solo 5 cosas sobre mí</h3>
-                    <p class="mt-4">Lo que menos me gusta de la programación es el trabajo en equipo. Y
-                        lo que más me gusta es trabajar en equipo.</p>
-                    <p class="mt-4">Veo menos que Pepe Leches pero no uso gafas. Si me ves alguna vez con
-                        ellas, es para aparentar que sé programar el doble.</p>
-                    <p class="mt-4">En una caída patinando, unos veinteañeros me preguntaron: "¿Se encuentra usted bien, señor?". Me dolió más que me llamaran señor que los 6 meses sin poder mover el hombro.</p>
-                    <p class="mt-4">Con un buen frontend y una hoja de Excel puedes vender un servicio online. Con el backend más limpio y escalable, sin un buen frontend, no tienes nada.</p>
-                    <p class="mt-4">Me repito constantemente a mí mismo la frase "La vida no premia a quien se esfuerza sino a quien consigue resultados".</p>
+                    <p class="mt-4">
+                        Lo que menos me gusta de la programación es el trabajo en equipo. Y lo que más me gusta es
+                        trabajar en equipo.
+                    </p>
+                    <p class="mt-4">
+                        Veo menos que Pepe Leches pero no uso gafas. Si me ves alguna vez con ellas, es para aparentar
+                        que sé programar el doble.
+                    </p>
+                    <p class="mt-4">
+                        En una caída patinando, unos veinteañeros me preguntaron: "¿Se encuentra usted bien, señor?". Me
+                        dolió más que me llamaran señor que los 6 meses sin poder mover el hombro.
+                    </p>
+                    <p class="mt-4">
+                        Con un buen frontend y una hoja de Excel puedes vender un servicio online. Con el backend más
+                        limpio y escalable, sin un buen frontend, no tienes nada.
+                    </p>
+                    <p class="mt-4">
+                        Me repito constantemente a mí mismo la frase "La vida no premia a quien se esfuerza sino a quien
+                        consigue resultados".
+                    </p>
                 </div>
             </div>
         </div>
