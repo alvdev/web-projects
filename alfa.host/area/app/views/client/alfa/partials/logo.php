4c6e1b57687a7fa17f8b65caf62d06175bd4ff22
alv.dev <soy@alv.dev>
2025-01-16 23:31:42 +0100

    fix logo require malfunction

--- alfa.host/area/app/views/client/alfa/partials/logo.php
+++ alfa.host/area/app/views/client/alfa/partials/logo.php
@@ -2,7 +2,7 @@
     <img <?= (!empty($client_logo_height) ? ' style="height: ' . $client_logo_height . 'px;"' : ''); ?> src="<?php (print(isset($theme_logo) ? $this->Html->safe($theme_logo) : null)); ?>"
         alt="<?= (isset($system_company->name) ? $system_company->name : null); ?>" />
 <?php elseif (!empty($blesta_logo)): ?>
-    <?= require(dirname(__DIR__, 1) . '/images/logo.svg') ?>
+    <?php require(dirname(__DIR__, 1) . '/images/logo.svg') ?>
 
 <?php else: ?>
     <img <?= (!empty($client_logo_height) ? ' style="height: ' . $client_logo_height . 'px;"' : ''); ?> class="blesta"
