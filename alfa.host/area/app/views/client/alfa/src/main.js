2613e2ba31bb3fb965be7b8c998dff059f3d34fe
alv.dev <soy@alv.dev>
2025-02-10 16:43:58 +0100

    add alpine collapse plugin

--- alfa.host/area/app/views/client/alfa/src/main.js
+++ alfa.host/area/app/views/client/alfa/src/main.js
@@ -1,5 +1,8 @@
 import Alpine from '../node_modules/alpinejs/dist/module.esm.min.js';
+import collapse from '../node_modules/@alpinejs/collapse/dist/module.esm.min.js';
 
 window.Alpine = Alpine;
+
+Alpine.plugin(collapse);
 
 Alpine.start();
