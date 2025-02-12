828fb429a0fa4f8f8d3152c30770a3726683d4b6
alv.dev <soy@alv.dev>
2025-02-12 15:48:39 +0100

    add black header instead of blue

--- alfa.host/area/app/views/client/alfa/partials/header.php
+++ alfa.host/area/app/views/client/alfa/partials/header.php
@@ -1,4 +1,4 @@
-<header class="relative bg-blue-700 shadow-2xl shadow-gray-500 before:absolute before:w-6 before:h-6 before:left-0 before:-bottom-6 before:shadow-[-0.25rem_-0.25rem_0_0.25rem_#093eba] before:rounded-tl-[1.5rem] after:absolute after:w-6 after:h-6 after:right-0 after:-bottom-6 after:shadow-[0.25rem_-0.25rem_0_0.25rem_#0037ad] after:rounded-tr-[3rem]">
+<header class="relative bg-blue-700 text-white/95 shadow-2xl shadow-gray-500 before:absolute before:w-6 before:h-6 before:left-0 before:-bottom-6 before:shadow-[-0.25rem_-0.25rem_0_0.25rem_#000102] before:rounded-tl-[1.5rem] after:absolute after:w-6 after:h-6 after:right-0 after:-bottom-6 after:shadow-[0.25rem_-0.25rem_0_0.25rem_#000] after:rounded-tr-[3rem]">
 
     <?php require_once('menu_top.php'); ?>
     <?php require_once('menu_main.php'); ?>
@@ -29,9 +29,9 @@
     <section class="cards h-0 overflow-clip container flex flex-col items-center relative bg-white rounded-sm lg:flex-row gap-x-8 gap-y-4"></section>
 </header>
 
-
 <?php
-$this->Javascript->setInline('
+// TODO: Need to fix an infinite loop issue with this code in login pages
+/* $this->Javascript->setInline('
     $(document).ready(function() {        
         $(".cards").blestaLoadCards({
             fetch_uri: "' . $this->Html->safe($this->base_uri . 'main/getcards/' . (isset($client->id) ? $client->id : null), true) . '",
@@ -45,5 +45,5 @@ $this->Javascript->setInline('
                 cardsElement.classList.add("flex", "-mt-16", "p-4", "translate-y-14", "h-auto", "transition-all");
             }
     }, 1000);
-');
+'); */
 ?>
