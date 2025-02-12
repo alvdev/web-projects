58a309ebb39d77e8421a4c23fe004d70bbd63cef
alv.dev <soy@alv.dev>
2025-02-12 15:49:54 +0100

    add separated alpine components file

---
+++ alfa.host/area/app/views/client/alfa/src/components.js
@@ -0,0 +1,6 @@
+import Alpine from '../node_modules/alpinejs/dist/module.esm.min.js';
+
+Alpine.data('testing', () => ({
+  show: false,
+  name: 'alvdev',
+}));
--- alfa.host/area/app/views/client/alfa/src/main.js
+++ alfa.host/area/app/views/client/alfa/src/main.js
@@ -1,5 +1,6 @@
 import Alpine from '../node_modules/alpinejs/dist/module.esm.min.js';
 import collapse from '../node_modules/@alpinejs/collapse/dist/module.esm.min.js';
+import './components.js';
 
 window.Alpine = Alpine;
 
