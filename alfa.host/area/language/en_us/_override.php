bcd18b945f530c5f0e2695dde067e3b56867bda6
alv.dev <soy@alv.dev>
2025-02-20 21:06:49 +0100

    add en_us override definitions

---
+++ alfa.host/area/language/en_us/_override.php
@@ -0,0 +1,19 @@
+<?php
+// Payment Types
+# REQUIRED FORMAT: $lang['_PaymentTypes.payment_type_here'] = "The payment type.";
+$lang['_PaymentTypes.cash'] = 'Cash';
+$lang['_PaymentTypes.check'] = 'Check';
+$lang['_PaymentTypes.money_order'] = 'Money Order';
+$lang['_PaymentTypes.in_house_credit'] = 'In House Credit';
+
+
+// Contact Types
+# REQUIRED FORMAT: $lang['_ContactTypes.contact_type_name_here'] = "The contact type.";
+
+
+// Custom Fields
+# REQUIRED FORMAT: $lang['_CustomFields.custom_field_name_here'] = "The custom field.";
+
+
+// Knowledgebase
+$lang['Knowledgebase.!text.root_directory'] = 'Categories';
