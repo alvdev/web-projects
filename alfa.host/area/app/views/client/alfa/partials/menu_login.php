3295bab8411f519f33e678db5761421aba5cb8ba
alv.dev <soy@alv.dev>
2025-03-06 08:36:32 +0100

    fix: user button was overriden by btn class

--- alfa.host/area/app/views/client/alfa/partials/menu_login.php
+++ alfa.host/area/app/views/client/alfa/partials/menu_login.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-2 relative">
     <?php if ((isset($logged_in) ? $logged_in : null)): ?>
-        <button class="nav-link btn-outline border-white text-white [&_svg]:fill-white hover:border-white hover:text-white hover:bg-white/10 [anchor-name:--user-menu-btn]" popovertarget="user-menu">
+        <button class="nav-link btn-outline bg-transparent bg-none rounded-full border-white text-white [&_svg]:fill-white hover:border-white hover:text-white hover:bg-white/10 [anchor-name:--user-menu-btn]" popovertarget="user-menu">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
                 <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
                 <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"></path>
