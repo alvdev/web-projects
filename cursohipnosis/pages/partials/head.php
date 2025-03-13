91ba83bc7e3705b2dc975182bbf3c5bd6d4c4c3b
alv.dev <soy@alv.dev>
2025-03-13 12:49:27 +0100

    refactor: improve functions naming

--- cursohipnosis/pages/partials/head.php
+++ cursohipnosis/pages/partials/head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= getTitle() ?></title>
-    <link rel="stylesheet" href="<?= assetUrl('main.min.css') ?>">
-    <script src="<?= assetUrl('flowbite.min.js') ?>"></script>
+    <title><?= title() ?></title>
+    <link rel="stylesheet" href="<?= asset('main.min.css') ?>">
+    <script src="<?= asset('flowbite.min.js') ?>"></script>
 </head>
