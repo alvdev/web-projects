8f14de0c6935b08d162f549d77c7e776d841820c
alv.dev <soy@alv.dev>
2025-03-13 18:43:05 +0100

    add landing intro sections

--- cursohipnosis/pages/sections/intro.php
+++ cursohipnosis/pages/sections/intro.php
@@ -1,3 +1,62 @@
-<section id="intro">
-    Section intro
+<section id="intro" class="container mt-36">
+    <div class="flex gap-24 justify-between items-center *:w-1/2">
+        <hgroup class="text-center">
+            <h1>Con el curso de hipnosis terapéutica <span class="font-bold">ayudarás a tus pacientes a curar</span>:</h1>
+            <h2 class="mt-8 opacity-70 text-3xl">fobias, ansiedad, nervios, insomnio, autoestima, dolores, estrés, depresión, y a dejar de fumar</h2>
+            <div class="flex justify-center gap-4 mt-16">
+                <a href="#contact" class="bg-blue-950 text-white px-8 py-4 rounded-lg uppercase flex items-center">
+                    Reservar plaza
+                </a>
+                <a href="#contact" class="border-1.5 px-8 py-4 rounded-lg uppercase *:[svg]:w-8 **:[path]:fill-blue-950">
+                    <?= asset('icons/whatsapp.svg', true) ?>
+                </a>
+                <a href="#contact" class="border-1.5 px-8 py-4 rounded-lg uppercase *:[svg]:w-8 **:[path]:fill-blue-950">
+                    <?= asset('icons/phone-calling.svg', true) ?>
+                </a>
+            </div>
+        </hgroup>
+
+        <div class="grid grid-cols-2 gap-8 *:border-4 *:border-white *:ring-3 *:rounded-lg *:relative *:shadow-2xl *:shadow-blue-100">
+            <img class="z-[4]" src="<?= asset('images/dejar-de-fumar-con-hipnosis.webp') ?>" alt="Imagen Curso de Hipnosis" />
+            <img class="z-[3] -top-8" src="<?= asset('images/durmiendo-gracias-a-la-hipnosis.webp') ?>" alt="Imagen Curso de Hipnosis" />
+            <img class="z-[5]" src="<?= asset('images/mujer-hipnotizada.webp') ?>" alt="Imagen Curso de Hipnosis" />
+            <img class="z-[6] -top-8" src="<?= asset('images/reduccion-de-medicamentos-con-hipnosis.webp') ?>" alt="Imagen Curso de Hipnosis" />
+        </div>
+    </div>
+</section>
+
+<section class="mt-48 relative after:absolute after:-z-10 after:inset-0 after:my-auto after:bg-blue-950 after:h-5/6 after:w-full after:border-4  after:content-['']">
+    <div class="container w-1/2 rounded-2xl overflow-clip border-white ring-8 border-6 shadow-2xl">
+        <iframe width="100%" height="" class="aspect-video" src="https://www.youtube.com/embed/EbcM5pXWbK8?si=UKkaaaZfbex0wpzo&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+</section>
+
+<section class="container mt-42">
+    <div class="text-center">
+        <h2 class="mb-8 text-5xl">¿<span class="font-bold">Quién te enseñará</span> la terapida de hipnosis?</h2>
+        <div class="text-2xl *:text-balance">
+            <p>Soy Jaime Velasco, especializado en hipnosis terapéutica, con más de 15 años de experiencia ayudando a pacientes con cualquier tipo de situación de las mencionadas.</p>
+            <p>Me encargaré de transmitirte tanto la información teórica como las técnicas a aplicar en el proceso de hipnosis, en función de tus necesidades y de tu perfil.</p>
+        </div>
+    </div>
+
+    <div class="mt-32 grid grid-cols-4 gap-8 justify-between items-center *:bg-blue-950 **:[p]:text-white **:[p]:opacity-100 *:h-full">
+        <img src="<?= asset('images/jaime-velasco.webp') ?>" alt="" class="border-4 rounded-lg">
+        <div class="flex flex-col justify-center p-12 rounded-lg">
+            <p>La Hipnosis Terapéutica es una técnica utilizada para ayudar a las personas a explorar y abordar diversos problemas de salud mental, emocionales o físicos.</p>
+        </div>
+
+        <div class="flex flex-col justify-center p-12 rounded-lg">
+            <p>Se basa en la inducción de un estado alterado de conciencia, conocido como trance hipnótico, en el cual la persona se vuelve más receptiva a las sugestiones y puede acceder a niveles más profundos de su mente.</p>
+        </div>
+
+        <div class="flex flex-col justify-center p-12 rounded-lg">
+            <p>En este curso, te acompañaré en el proceso de aprendizaje para que puedas llegar a ayudar a modificar patrones de pensamiento, comportamientos no deseados o emociones negativas de tus pacientes.</p>
+        </div>
+    </div>
+</section>
+
+<section class="container mt-36 text-center">
+    <h2 class="text-5xl">¿No quieres perder más tiempo?</h2>
+    <a href="#contact" class="mt-16 border-2 inline-flex items-center justify-center min-w-1/2 py-8 text-2xl font-semibold rounded-lg uppercase">Reserva tu plaza ya</a>
 </section>
