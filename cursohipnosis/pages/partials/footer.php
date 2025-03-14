38d33f6ccb4b048774df4e0d253e41a500eb823e
alv.dev <soy@alv.dev>
2025-03-14 17:36:24 +0100

    add landing footer section

--- cursohipnosis/pages/partials/footer.php
+++ cursohipnosis/pages/partials/footer.php
@@ -1,3 +1,10 @@
-<footer>
-    The footer
+<footer class="mt-42">
+    <div class="container border-t py-16 flex items-center justify-between">
+        <p>
+            Jaime Velasco © Copyright <?= date('Y') ?>
+        </p>
+        <ul>
+            <li>Política de privadidad</li>
+        </ul>
+    </div>
 </footer>
