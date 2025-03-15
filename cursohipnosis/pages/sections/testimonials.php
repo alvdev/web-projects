e029a261f22b4e4209d3e90968ab8c080e03fbd3
alv.dev <soy@alv.dev>
2025-03-15 22:17:50 +0100

    add responsive testimonials section

--- cursohipnosis/pages/sections/testimonials.php
+++ cursohipnosis/pages/sections/testimonials.php
@@ -1,18 +1,18 @@
-<section id="testimonials" class="mt-48 scroll-mt-48">
+<section id="testimonials" class="mt-24 md:mt-36lg:mt-48 scroll-mt-56 md:scroll-mt-48">
     <hgroup class="container text-center">
-        <h2 class="text-5xl"><span class="font-bold">Testimonios reales</span> tras<br>sesión de hipnosis terapéutica</h2>
+        <h2 class="text-4xl md:text-5xl lg:w-2/3 mx-auto"><span class="font-bold">Testimonios reales</span> tras sesión de hipnosis terapéutica</h2>
         <p class="mt-8 opacity-70 text-3xl"></p>
     </hgroup>
 
-    <div class="mt-24 relative after:absolute after:-z-10 after:inset-0 after:my-auto after:bg-blue-950 after:h-5/6 after:w-full after:content-['']">
-        <div class="container grid grid-cols-3 gap-8 *:border-4 *:border-white *:ring-4 *:rounded-lg *:overflow-clip *:shadow-2xl *:shadow-blue-500/30 *:relative *:bg-blue-950">
+    <div class="mt-12 md:mt-16 lg:mt-24 relative after:absolute after:-z-10 after:inset-0 after:my-auto after:bg-blue-950 after:h-5/6 after:w-full after:content-['']">
+        <div class="container grid grid-cols-1 md:grid-cols-3 gap-8 *:border-4 *:border-white *:ring-4 *:rounded-lg *:overflow-clip *:shadow-2xl *:shadow-blue-500/30 *:relative *:bg-blue-950">
             <div class="">
                 <lite-youtube videoid="I2qLsOaI-34"></lite-youtube>
             </div>
             <div class="">
                 <lite-youtube videoid="eBgSVUzQvpo"></lite-youtube>
             </div>
-            <div class="col-start-2 row-start-1 row-span-2 flex my-8">
+            <div class="md:col-start-2 md:row-start-1 md:row-span-2 flex md:my-8">
                 <lite-youtube videoid="0WgO-K6gNDc"></lite-youtube>
             </div>
             <div>
@@ -25,19 +25,19 @@
     </div>
 </section>
 
-<section class="container mt-48">
-    <h2 class="text-5xl text-center">Juan Antonio <span class="font-bold">realizó el curso</span> de hipnosis terapéutica con Jaime Velasco</h2>
-    <div class="mt-24 flex items-center gap-24 *:w-1/2">
+<section class="container mt-24 md:mt-36 lg:mt-48">
+    <h2 class="md:text-5xl text-center">Juan Antonio <span class="font-bold">realizó el curso</span> de hipnosis terapéutica con Jaime Velasco</h2>
+    <div class="mt-12 md:mt-16 lg:mt-24 grid lg:flex items-center gap-12 md:gap-16 lg:gap-24 lg:*:w-1/2">
         <div class="*:border-4 *:border-white *:ring-4 *:rounded-lg *:overflow-clip *:shadow-2xl *:shadow-blue-500/30 *:relative *:bg-blue-950">
             <lite-youtube videoid="_0zqPEgWxw8"></lite-youtube>
         </div>
 
         <div class="text-center">
-            <p class="opacity-70 text-3xl">
+            <p class="opacity-70 text-2xl md:text-3xl">
                 Si quieres aprender la técnica de hipnosis terapéutica como Juan Antonio, reserva tu cita lo antes posible.
             </p>
-            <p>* Las plazas están muy limitadas.</p>
-            <a href="#contact" class="mt-16 border-2 inline-flex items-center justify-center min-w-1/2 py-8 text-2xl font-semibold rounded-lg uppercase">Pedir cita ahora</a>
+            <p>* Las plazas están <u>muy limitadas</u>.</p>
+            <a href="#contact" class="mt-12 md:mt-16 border-2 inline-flex items-center justify-center min-w-1/2 py-6 md:py-8 px-12 text-xl md:text-2xl font-semibold rounded-lg uppercase">Pedir cita ahora</a>
         </div>
     </div>
 </section>
