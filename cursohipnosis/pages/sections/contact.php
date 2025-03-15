c4e16815c97e0250f81b1a463c2e07babb31fb4f
alv.dev <soy@alv.dev>
2025-03-15 15:34:08 +0100

    add whatsapp/phone links

--- cursohipnosis/pages/sections/contact.php
+++ cursohipnosis/pages/sections/contact.php
@@ -18,11 +18,11 @@
             </div>
 
             <div class="mt-12 flex justify-center gap-4 text-white uppercase text-xl *:w-full">
-                <a href="#contact" class="flex items-center justify-center gap-4 bg-blue-950 pl-4 pr-8 py-4 rounded-lg *:[svg]:w-8 **:[path]:fill-white">
+                <a href="tel:+34630818123" class="flex items-center justify-center gap-4 bg-blue-950 pl-4 pr-8 py-4 rounded-lg *:[svg]:w-8 **:[path]:fill-white">
                     <?= asset('icons/phone-calling.svg', true) ?>
                     Llamar por teléfono
                 </a>
-                <a href="#contact" class="flex items-center justify-center gap-4 bg-blue-950 pl-4 pr-8 py-4 rounded-lg *:[svg]:w-8 **:[path]:fill-white">
+                <a href="https://wa.me/34630818123" class="flex items-center justify-center gap-4 bg-blue-950 pl-4 pr-8 py-4 rounded-lg *:[svg]:w-8 **:[path]:fill-white">
                     <?= asset('icons/whatsapp.svg', true) ?>
                     Escribir por Whatsapp
                 </a>
@@ -39,6 +39,7 @@
                         title="Solo se permiten letras y espacios" required>
                     <div class="error-message" data-field="name"></div>
                 </div>
+
                 <div class="field email relative">
                     <i class="absolute z-10 w-8 h-8 top-1/2 -translate-y-1/2 left-4">
                         <?= asset('icons/at.svg', true) ?>
@@ -47,6 +48,7 @@
                         placeholder="Correo Electrónico" required>
                     <div class="error-message" data-field="email"></div>
                 </div>
+
                 <div class="field phone relative">
                     <i class="absolute z-10 w-8 h-8 top-1/2 -translate-y-1/2 left-4">
                         <?= asset('icons/phone.svg', true) ?>
@@ -54,12 +56,14 @@
                     <input type="tel" name="phone" placeholder="Teléfono" pattern="\d{9}" maxlength="9" required>
                     <div class="error-message" data-field="phone"></div>
                 </div>
+
                 <div class="field website hidden">
                     <i class="absolute z-10 w-8 h-8 top-1/2 -translate-y-1/2 left-4">
                         <?= asset('icons/user.svg', true) ?>
                     </i>
                     <input type="text" name="website" placeholder="Website">
                 </div>
+                
                 <div class="field message relative">
                     <i class="absolute z-10 w-8 h-8 top-4 left-4">
                         <?= asset('icons/question.svg', true) ?>
@@ -68,8 +72,9 @@
                         placeholder="¿Por qué te interesa el curso?"></textarea>
                     <div class="error-message" data-field="message"></div>
                 </div>
-                
+
                 <div id="response-banner" class="text-lime-400! text-center text-balance text-2xl"></div>
+
                 <button type="submit" class="rounded-lg px-8 py-6 bg-blue-400 text-3xl font-semibold drop-shadow-xl uppercase [text-shadow:1px_1px_5px_rgba(0,0,0,0.3)]">Reservar plaza</button>
             </form>
         </div>
