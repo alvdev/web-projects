84e5ab1af7fbe8eecb148ac77e19764bf8600b04
alv.dev <soy@alv.dev>
2025-03-15 16:34:57 +0100

    add responsive intro section

--- cursohipnosis/pages/sections/intro.php
+++ cursohipnosis/pages/sections/intro.php
@@ -1,22 +1,24 @@
-<section id="intro" class="container mt-36 scroll-mt-84">
-    <div class="flex gap-24 justify-between items-center *:w-1/2">
+<section id="intro" class="container mt-16 md:mt-24 lg:mt-36 scroll-mt-84">
+    <div class="lg:flex gap-24 justify-between items-center lg:*:w-1/2">
         <hgroup class="text-center">
             <h1>Con el curso de hipnosis terapéutica <span class="font-bold">ayudarás a tus pacientes a curar</span>:</h1>
-            <h2 class="mt-8 opacity-70 text-3xl">fobias, ansiedad, nervios, insomnio, autoestima, dolores, estrés, depresión y tabaquismo.</h2>
-            <div class="flex justify-center gap-4 mt-16">
+            <h2 class="mt-8 opacity-70 text-2xl md:text-3xl">fobias, ansiedad, nervios, insomnio, autoestima, dolores, estrés, depresión y tabaquismo.</h2>
+            <div class="w-fit md:w-full mx-auto md:flex justify-center gap-4 mt-16">
                 <a href="#contact" class="bg-blue-950 text-white px-8 py-4 rounded-lg uppercase flex items-center">
                     Reservar plaza
                 </a>
-                <a href="https://wa.me/34630818123" class="border-1.5 px-8 py-4 rounded-lg uppercase *:[svg]:w-8 **:[path]:fill-blue-950">
-                    <?= asset('icons/whatsapp.svg', true) ?>
-                </a>
-                <a href="tel:+34630818123" class="border-1.5 px-8 py-4 rounded-lg uppercase *:[svg]:w-8 **:[path]:fill-blue-950">
-                    <?= asset('icons/phone-calling.svg', true) ?>
-                </a>
+                <div class="flex gap-4 mt-4 md:mt-0">
+                    <a href="https://wa.me/34630818123" class="inline-flex border-1.5 px-8 py-4 rounded-lg uppercase *:[svg]:w-8 **:[path]:fill-blue-950">
+                        <?= asset('icons/whatsapp.svg', true) ?>
+                    </a>
+                    <a href="tel:+34630818123" class="inline-flex border-1.5 px-8 py-4 rounded-lg uppercase *:[svg]:w-8 **:[path]:fill-blue-950">
+                        <?= asset('icons/phone-calling.svg', true) ?>
+                    </a>
+                </div>
             </div>
         </hgroup>
 
-        <div class="grid grid-cols-2 gap-8 *:border-4 *:border-white *:ring-3 *:rounded-lg *:relative *:shadow-2xl *:shadow-blue-100">
+        <div class="mt-24 md:mt-32 lg:mt-0 grid grid-cols-2 gap-4 md:gap-8 *:border-4 *:border-white *:ring-3 *:rounded-lg *:relative *:shadow-2xl *:shadow-blue-100">
             <img class="z-[4]" src="<?= asset('images/dejar-de-fumar-con-hipnosis.webp') ?>" alt="Imagen Curso de Hipnosis" />
             <img class="z-[3] -top-8" src="<?= asset('images/durmiendo-gracias-a-la-hipnosis.webp') ?>" alt="Imagen Curso de Hipnosis" />
             <img class="z-[5]" src="<?= asset('images/mujer-hipnotizada.webp') ?>" alt="Imagen Curso de Hipnosis" />
@@ -25,22 +27,22 @@
     </div>
 </section>
 
-<section class="mt-48 relative after:absolute after:-z-10 after:inset-0 after:my-auto after:bg-blue-950 after:h-5/6 after:w-full after:border-4  after:content-['']">
-    <div class="container w-1/2 rounded-2xl overflow-clip border-white ring-8 border-6 shadow-2xl">
+<section class="mt-24 md:mt-36 lg:mt-48 relative after:absolute after:-z-10 after:inset-0 after:my-auto after:bg-blue-950 after:h-5/6 after:w-full after:border-4  after:content-['']">
+    <div class="container w-5/6 lg:w-1/2 rounded-2xl overflow-clip border-white ring-8 border-6 shadow-2xl">
         <lite-youtube videoid="EbcM5pXWbK8"></lite-youtube>
     </div>
 </section>
 
-<section class="container mt-42">
+<section class="container mt-24 md:mt-36 lg:mt-42">
     <div class="text-center">
-        <h2 class="mb-8 text-5xl">¿<span class="font-bold">Quién te enseñará</span> la terapia de hipnosis?</h2>
+        <h2 class="mb-8 text-4xl md:text-5xl">¿<span class="font-bold">Quién te enseñará</span> la terapia de hipnosis?</h2>
         <div class="text-2xl *:text-balance">
             <p>Soy Jaime Velasco, especializado en hipnosis terapéutica, con más de 15 años de experiencia ayudando a pacientes con cualquier tipo de situación de las mencionadas.</p>
             <p>Me encargaré de transmitirte tanto la información teórica como las técnicas a aplicar en el proceso de hipnosis, en función de tus necesidades y de tu perfil.</p>
         </div>
     </div>
 
-    <div class="mt-32 grid grid-cols-4 gap-8 justify-between items-center *:bg-blue-950 **:[p]:text-white **:[p]:opacity-100 *:h-full">
+    <div class="mt-16 md:mt-24 lg:mt-32 grid md:grid-cols-2 lg:grid-cols-4 gap-8 justify-between items-center *:bg-blue-950 **:[p]:text-white **:[p]:opacity-100 *:h-full">
         <img loading="lazy" src="<?= asset('images/jaime-velasco.webp') ?>" alt="" class="border-4 rounded-lg">
         <div class="flex flex-col justify-center p-12 rounded-lg">
             <p>La hipnosis terapéutica es una técnica utilizada para ayudar a las personas a explorar y abordar diversos problemas de salud mental, emocionales o físicos.</p>
@@ -56,7 +58,7 @@
     </div>
 </section>
 
-<section class="container mt-36 text-center">
-    <h2 class="text-5xl">¿No quieres perder más tiempo?</h2>
-    <a href="#contact" class="mt-16 border-2 inline-flex items-center justify-center min-w-1/2 py-8 text-2xl font-semibold rounded-lg uppercase">Reserva tu plaza ya</a>
+<section class="container mt-16 md:mt-24 lg:mt-36 text-center">
+    <h2 class="text-4xl md:text-5xl">¿No quieres perder más tiempo?</h2>
+    <a href="#contact" class="mt-8 md:mt-12 lg:mt-16 border-2 inline-flex items-center justify-center md:min-w-1/2 px-12 py-8 text-2xl font-semibold rounded-lg uppercase">Reserva tu plaza ya</a>
 </section>
