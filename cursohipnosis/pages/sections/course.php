3ec6713282164502554793188274b60a8cd3516a
alv.dev <soy@alv.dev>
2025-03-15 23:28:22 +0100

    add responsive course section

--- cursohipnosis/pages/sections/course.php
+++ cursohipnosis/pages/sections/course.php
@@ -1,42 +1,42 @@
-<section id="course" class="relative text-5xl text-center">
-    <div class="grid">
-        <div class="justify-self-end relative top-20">
+<section id="course" class="relative text-center">
+    <div class="grid overflow-clip">
+        <div class="justify-self-end relative -right-24 md:-right-36 lg:right-0 top-12 md:top-24 lg:top-20">
             <img src="<?= asset('images/hand-1.webp') ?>" alt="">
         </div>
-        <h2 class="container text-5xl text-center">
+        <h2 class="container  text-3xl md:text-4xl lg:text-5xl text-center">
             <span class="font-bold">El Curso de Hipnosis Terapeutica te ayudará</span> a complementar tus conocimientos y a mejorar en la forma de tratar los problemas de tus pacientes.
         </h2>
-        <div class="mt-8">
+        <div class="mt-8 relative -left-16 md:left-0">
             <img src="<?= asset('images/hand-2.webp') ?>" alt="">
         </div>
     </div>
 </section>
 
 <section>
-    <div class="container grid grid-cols-4 gap-8 items-center">
-        <div class="text-center bg-blue-950 text-white self-stretch flex flex-col items-center justify-center gap-2 rounded-lg">
+    <div class="container grid md:grid-cols-2 lg:grid-cols-4 gap-8 items-center">
+        <div class="text-center bg-blue-950 text-white self-stretch flex flex-col items-center justify-center gap-2 px-4 py-12 rounded-lg">
             <div class="text-xl uppercase font-semibold">Duración</div>
             <div class="text-3xl font-light">3 días consecutivos</div>
         </div>
-        <div class="border border-blue-950 rounded-lg px-6 py-4">
+        <div class="border border-blue-950 rounded-lg px-7 py-5 text-balance">
             Durante el curso, se entregará la teoría, práctica y aprendizaje necesarios para la comprensión total de un proceso de hipnosis, sus fases y cómo afrontar cada una de ellas.
         </div>
-        <div class="text-center bg-blue-950 text-white self-stretch flex flex-col items-center justify-center gap-2 rounded-lg">
+        <div class="text-center bg-blue-950 text-white self-stretch flex flex-col items-center justify-center gap-2 px-4 py-12 rounded-lg">
             <div class="text-xl uppercase font-semibold">Modalidad</div>
             <div class="text-3xl font-light">Presencial</div>
         </div>
-        <div class="border border-blue-950 rounded-lg px-6 py-4">
+        <div class="border border-blue-950 rounded-lg px-7 py-5 text-balance">
             Después de superar esta formación, dispondrás de todas las herramientas para lograr la efectividad total en cada sesión de hipnosis terapéutca que realices con tus pacientes.
         </div>
     </div>
 
-    <div class="container mt-8 grid grid-cols-4 gap-8 items-center">
+    <div class="container mt-8 grid md:grid-cols-2 lg:grid-cols-4 gap-8 items-center">
         <div class="relative">
             <img loading="lazy" src="<?= asset('images/fases-curso-hipnosis-1.webp') ?>" alt="" class="rounded-lg">
             <div class="absolute z-10 flex flex-col justify-between h-full w-full top-0 p-8 *:fill-white ">
                 <?= asset('icons/check-note.svg', true) ?>
-                <h3 class="text-white text-4xl flex items-center gap-4">
-                    <span class="text-8xl">1</span>
+                <h3 class="text-white text-4xl lg:text-xl xl:text-[1.6rem] flex items-center gap-4">
+                    <span class="text-7xl">1</span>
                     Evaluación preliminar
                 </h3>
             </div>
@@ -45,8 +45,8 @@
             <img loading="lazy" src="<?= asset('images/fases-curso-hipnosis-2.webp') ?>" alt="" class="rounded-lg">
             <div class="absolute z-10 flex flex-col justify-between h-full w-full top-0 p-8 *:fill-white ">
                 <?= asset('icons/road-plan.svg', true) ?>
-                <h3 class="text-white text-4xl flex items-center gap-4">
-                    <span class="text-8xl">2</span>
+                <h3 class="text-white text-4xl lg:text-xl xl:text-[1.6rem] flex items-center gap-4">
+                    <span class="text-7xl">2</span>
                     Planificación personalizada
                 </h3>
             </div>
@@ -55,8 +55,8 @@
             <img loading="lazy" src="<?= asset('images/fases-curso-hipnosis-3.webp') ?>" alt="" class="rounded-lg">
             <div class="absolute z-10 flex flex-col justify-between h-full w-full top-0 p-8 *:fill-white ">
                 <?= asset('icons/road-indicators.svg', true) ?>
-                <h3 class="text-white text-4xl flex items-center gap-4">
-                    <span class="text-8xl">3</span>
+                <h3 class="text-white text-4xl lg:text-xl xl:text-[1.6rem] flex items-center gap-4">
+                    <span class="text-7xl">3</span>
                     Orientación constante
                 </h3>
             </div>
@@ -65,8 +65,8 @@
             <img loading="lazy" src="<?= asset('images/fases-curso-hipnosis-4.webp') ?>" alt="" class="rounded-lg">
             <div class="absolute z-10 flex flex-col justify-between h-full w-full top-0 p-8 *:fill-white ">
                 <?= asset('icons/growth-graph.svg', true) ?>
-                <h3 class="text-white text-4xl flex items-center gap-4">
-                    <span class="text-8xl">4</span>
+                <h3 class="text-white text-4xl lg:text-xl xl:text-[1.6rem] flex items-center gap-4">
+                    <span class="text-7xl">4</span>
                     Seguimiento de resultados
                 </h3>
             </div>
@@ -74,57 +74,57 @@
     </div>
 </section>
 
-<section class="mt-42">
-    <h2 class="container text-5xl text-center">
+<section class="mt-24 md:mt-36 lg:mt-42">
+    <h2 class="container text-4xl md:text-5xl text-center">
         ¿<span class="font-bold">Por qué realizar el curso</span> de hipnosis terapéutica?
     </h2>
-    <div class="container mt-24 grid grid-cols-2 gap-8">
-        <div class="flex flex-col gap-8">
-            <details class="" name="faq">
+    <div class="container mt-12 md:mt-20 lg:mt-24 grid md:grid-cols-2 gap-6 md:gap-8">
+        <div class="flex flex-col gap-6 md:gap-8">
+            <details name="faq">
                 <summary>Amplía las opciones terapéuticas</summary>
                 <div>
                     La hipnosis terapéutica es una herramienta adicional que puede brindar a los terapeutas una mayor variedad de opciones para abordar los problemas de sus pacientes. Esto puede ser especialmente útil cuando otras terapias no han tenido éxito o cuando se requiere un enfoque diferente.
                 </div>
             </details>
-            <details class="" name="faq">
+            <details name="faq">
                 <summary>Acceso a recursos subconscientes</summary>
                 <div>
                     La hipnosis terapéutica permite acceder al subconsciente del paciente, lo que puede ayudar a identificar y abordar problemas arraigados que pueden ser difíciles de abordar con terapias tradicionales. Esto puede ser especialmente útil en el tratamiento de traumas, fobias y otros problemas emocionales profundos.
                 </div>
             </details>
-            <details class="" name="faq">
+            <details name="faq">
                 <summary>Potencia el autocontrol y el cambio de hábitos</summary>
                 <div>
                     La hipnosis terapéutica puede ser efectiva para ayudar a las personas a cambiar comportamientos no deseados o hábitos perjudiciales, como dejar de fumar, perder peso o superar adicciones.
                 </div>
             </details>
-            <details class="" name="faq">
+            <details name="faq">
                 <summary>Reducción del estrés y la ansiedad</summary>
                 <div>
                     La hipnosis terapéutica puede ayudar a las personas a relajarse, reducir el estrés y la ansiedad, y mejorar su bienestar general. Esto puede complementar otras terapias destinadas a tratar trastornos de ansiedad, depresión o estrés postraumático.
                 </div>
             </details>
         </div>
-        <div class="flex flex-col gap-8">
-            <details class="" name="faq">
+        <div class="flex flex-col gap-6 md:gap-8">
+            <details name="faq">
                 <summary>Aumento de la autoestima y la confianza</summary>
                 <div>
                     La hipnosis terapéutica puede ayudar a las personas a desarrollar una mayor autoestima y confianza en sí mismas, lo que puede ser beneficioso en el proceso de recuperación y en la mejora de su calidad de vida.
                 </div>
             </details>
-            <details class="" name="faq">
+            <details name="faq">
                 <summary>Enfoque en problemas específicos</summary>
                 <div>
                     La hipnosis terapéutica se puede adaptar para abordar problemas específicos de cada paciente, lo que la hace versátil y personalizable para las necesidades individuales.
                 </div>
             </details>
-            <details class="" name="faq">
+            <details name="faq">
                 <summary>Complemento de terapias tradicionales</summary>
                 <div>
                     La hipnosis terapéutica se puede utilizar en conjunto con otras terapias, como la psicoterapia cognitivo-conductual, la terapia de conversación o la terapia farmacológica. Esto puede aumentar las posibilidades de éxito en el tratamiento.
                 </div>
             </details>
-            <details class="" name="faq">
+            <details name="faq">
                 <summary>Resultados rápidos</summary>
                 <div>
                     En algunos casos, la hipnosis terapéutica puede producir resultados más rápidos en comparación con otras terapias, lo que puede ser alentador para los pacientes que buscan una mejoría rápida.
@@ -134,11 +134,11 @@
     </div>
 </section>
 
-<section class="mt-36 bg-blue-950 py-16 text-white">
-    <div class="container flex items-center gap-16 *:first:w-1/4 *:last:w-3/4">
-        <h2 class="text-5xl">Recuerda siempre que...</h2>
+<section class="mt-20 md:mt-28 lg:mt-36 bg-blue-950 py-16 text-white">
+    <div class="container flex flex-col md:flex-row items-baseline lg:items-center gap-16 md:*:first:w-1/4 md:*:last:w-3/4">
+        <h2 class="md:text-4xl lg:text-5xl md:sticky md:top-24 lg:static lg:top-0 w-full text-center md:text-left">Recuerda siempre que...</h2>
         <div>
-            <div class="grid grid-cols-3 gap-8 items-center justify-end *:opacity-100 *:h-full *:m-0 *:flex *:items-center *:px-8 *:py-6 *:border *:rounded-lg">
+            <div class="grid lg:grid-cols-3 gap-8 items-center justify-end *:opacity-100 *:h-full *:m-0 *:flex *:items-center *:px-8 *:py-6 *:border *:rounded-lg">
                 <p>
                     La hipnosis terapéutica debe ser realizada por profesionales debidamente capacitados y muy éticos en su práctica.
                 </p>
