f3ccb76c9b4e443b8ebbe1cc70a4a6725438a1a3
alv.dev <soy@alv.dev>
2025-03-18 01:49:35 +0100

    add privacy page

--- cursohipnosis/pages/partials/footer.php
+++ cursohipnosis/pages/partials/footer.php
@@ -1,10 +1,14 @@
 <footer class="mt-24 md:mt-32 lg:mt-42">
-    <div class="container *:mx-auto *:md:mx-0 border-t py-16 flex flex-wrap text-center  items-center justify-between">
-        <p>
+    <div class="container *:mx-auto *:md:mx-0 border-t py-16 flex flex-wrap text-center text-xl items-center justify-between">
+        <p class="text-xl">
             Jaime Velasco © Copyright <?= date('Y') ?>
         </p>
         <ul>
-            <li>Política de privadidad</li>
+            <li>
+                <a href="privacidad.php">
+                    Política de privadidad
+                </a>
+            </li>
         </ul>
     </div>
 </footer>
---
+++ cursohipnosis/pages/privacidad.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="es" class="scroll-smooth">
+<?php require('partials/head.php') ?>
+
+<body class="debug-screens">
+    <?php require('partials/header.php') ?>
+
+    <main class="container mt-16 md:mt-24 text-xl">
+        <h1 class="text-4xl">Protección de datos de carácter personal según el RGPD</h1>
+
+        <p class="text-xl mt-8">
+            Jaime Parriego Velasco como persona Jurídica, en aplicación de la normativa vigente en materia de protección de datos de
+            carácter personal, informa que los datos personales que se recogen a través de los formularios del Sitio web:
+        </p>
+
+        <p class="text-xl">www.cursohipnosis.com, se incluyen en los ficheros automatizados específicos de usuarios de los servicios de Jaime Velasco.</p>
+
+        <p class="text-xl">
+            La recogida y tratamiento automatizado de los datos de carácter personal tiene como finalidad el mantenimiento de la
+            relación comercial y el desempeño de tareas de información, formación, asesoramiento y otras actividades propias de
+            ZENOR S.L..
+        </p>
+
+        <p class="text-xl">
+            Estos datos únicamente serán cedidos a aquellas entidades que sean necesarias con el único objetivo de dar cumplimiento
+            a la finalidad anteriormente expuesta.
+        </p>
+
+        <p class="text-xl">
+            Jaime Parriego Velasco adopta las medidas necesarias para garantizar la seguridad, integridad y confidencialidad de los
+            datos conforme a lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de
+            2016, relativo a la protección de las personas físicas en lo que respecta al tratamiento de datos personales y a la
+            libre circulación de los mismos.
+        </p>
+
+        <p class="text-xl">
+            El usuario podrá en cualquier momento ejercitar los derechos de acceso, oposición, rectificación, cancelación,
+            limitación y portabilidad reconocidos en el citado Reglamento (UE).
+        </p>
+
+        <p class="text-xl">El ejercicio de estos derechos puede realizarlo el propio usuario a través de:</p>
+
+        <ul>
+            <li>Email a: info@cursohipnosis.com</li>
+
+            <li>En la dirección: C/Silva Nº4 1A 28013, Madrid.</li>
+
+            <li>Realizando una solicitud desde el formulario : SOLICITUD DE ELIMINACIÓN / MODIFICACIÓN DE DATOS</li>
+        </ul>
+
+        <p class="text-xl">
+            El usuario manifiesta que todos los datos facilitados por él son ciertos y correctos, y se compromete a mantenerlos
+            actualizados, comunicando los cambios a Jaime Parriego Velasco.
+        </p>
+
+        <h2 class="mt-12 text-3xl">Finalidad del tratamiento de los datos personales:</h2>
+
+        <p class="text-xl">¿Con qué finalidad trataremos tus datos personales?</p>
+
+        <p class="text-xl">En Curso Hipnosis, trataremos tus datos personales recabados a través del Sitio Web cursohipnosis.com, con las siguientes finalidades:</p>
+
+        <ul>
+            <li>Dar cumplimiento de las obligaciones, comerciales, laborales, corporativas y contables de la empresa.</li>
+    
+            <li>Prestar sus servicios de acuerdo con las necesidades particulares de los clientes, con el fin de cumplir los contratos suscritos por la misma.</li>
+    
+            <li>Envío de información comercial y boletines sobre nuevos servicios ofrecidos en la web y del sector.</li>
+    
+            <li>El proceso de archivo, de actualización de los sistemas, de protección y custodia de información y bases de datos de la empresa.</li>
+    
+            <li>Prestar los servicios contratados por el usuario.</li>
+    
+            <li>Enviar información promocional vía electrónica.</li>
+    
+            <li>Facilitar la información solicitada por el usuario a través del formulario de contacto.</li>
+        </ul>
+
+        <p class="text-xl">
+            Te recordamos que puedes oponerte al envío de comunicaciones comerciales por cualquier vía y en cualquier momento,
+            remitiendo un correo electrónico a la dirección anteriormente indicada.
+        </p>
+
+        <p class="text-xl">
+            Los campos de dichos registros son de cumplimentación obligatoria, siendo imposible realizar las finalidades expresadas
+            si no se aportan esos datos.
+        </p>
+
+        <h3 class="text-2xl mt-8">¿Por cuánto tiempo se conservan los datos personales recabados?</h3>
+
+        <p class="text-xl">Los datos personales proporcionados se conservarán mientras se mantenga la relación comercial o no solicites su supresión y durante el plazo por el cuál pudieran derivarse responsabilidades legales por los servicios prestados.</p>
+
+        <h2 class="mt-12">Legitimación</h2>
+
+        <p class="text-xl">El tratamiento de tus datos se realiza con las siguientes bases jurídicas que legitiman el mismo:</p>
+
+        <p class="text-xl">
+            La solicitud de información y/o la contratación de los servicios de Jaime Parriego Velasco, cuyos términos y condiciones
+            se pondrán a tu disposición en todo caso, de forma previa a una eventual contratación.
+        </p>
+
+        <p class="text-xl">
+            El consentimiento libre, específico, informado e inequívoco, en tanto que te informamos poniendo a tu disposición la
+            presente política de privacidad, que tras la lectura de la misma, en caso de estar conforme, puedes aceptar mediante una
+            declaración o una clara acción afirmativa, como el marcado de una casilla dispuesta al efecto.
+        </p>
+
+        <p class="text-xl">
+            En caso de que no nos facilites tus datos o lo hagas de forma errónea o incompleta, no podremos atender tu solicitud,
+            resultando del todo imposible proporcionarte la información solicitada o llevar a cabo la contratación de los servicios.
+        </p>
+
+        <h2 class="mt-12">Destinatarios</h2>
+
+        <p class="text-xl">Los datos no se comunicarán a ningún tercero ajeno a Jaime Parriego Velasco , salvo obligación legal.</p>
+
+        <p class="text-xl">
+            Curso Hipnosis informa de que, como prestador de servicio de alojamiento de datos y en virtud de lo establecido en la Ley
+            34/2002 de 11 de julio de Servicios de la Sociedad de la Información y de Comercio Electrónico (LSSI), retiene por un
+            periodo máximo de 12 meses la información imprescindible para identificar el origen de los datos alojados y el momento
+            en que se inició la prestación del servicio. La retención de estos datos no afecta al secreto de las comunicaciones y
+            sólo podrán ser utilizados en el marco de una investigación criminal o para la salvaguardia de laseguridad pública,
+            poniéndose a disposición de los jueces y/o tribunales o del Ministerio que así los requiera.
+        </p>
+
+        <p class="text-xl">
+            La comunicación de datos a las Fuerzas y Cuerpos del Estado se hará en virtud a lo dispuesto en la normativa sobre
+            protección de datos personales.
+        </p>
+
+        <h2 class="mt-12">Derechos propiedad intelectual cursohipnosis.com</h2>
+
+        <p class="text-xl">
+            No se permite la reproducción, publicación y/o uso no estrictamente privado de los contenidos, totales o parciales, del
+            sitio web cursohipnosis.com sin el consentimiento previo y por escrito.
+        </p>
+
+        <p class="text-xl">
+            La recogida y/o utilización de datos personales de otros usuarios sin su consentimiento expreso o contraviniendo lo
+            dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la
+            protección de las personas físicas en lo que respecta al tratamiento dedatos personales y a la libre circulación de los
+            mismos.
+        </p>
+
+        <p class="text-xl">
+            La utilización del servidor de correo del dominio y de las direcciones de correo electrónico para el envío de correo
+            masivo no deseado.
+        </p>
+
+        <p class="text-xl">
+            El usuario tiene toda la responsabilidad sobre el contenido de su web, la información transmitida y almacenada, los
+            enlaces de hipertexto, las reivindicaciones de terceros y las acciones legales en referencia a propiedad intelectual,
+            derechos de terceros y protección de menores.
+        </p>
+
+        <p class="text-xl">
+            El usuario es responsable respecto a las leyes y reglamentos en vigor y las reglas que tienen que ver con el
+            funcionamiento del servicio online, comercio electrónico, derechos de autor, mantenimiento del orden público, así como
+            principios universales de uso de Internet.
+        </p>
+
+        <p class="text-xl">
+            El usuario indemnizará a Jaime Parriego Velasco por los gastos que generara la imputación de Jaime Parriego Velasco en
+            alguna causa cuya responsabilidad fuera atribuible al usuario, incluidos honorarios y gastos de defensa jurídica,
+            incluso en el caso de una decisión judicial no definitiva.
+        </p>
+
+        <h2 class="mt-12">Protección de la información alojada</h2>
+
+        <p class="text-xl">
+            Curso Hipnosis realiza copias de seguridad de los contenidos alojados en sus servidores, sin embargo no se responsabiliza de
+            la pérdida o el borrado accidental de los datos por parte de los usuarios. De igual manera, no garantiza la reposición
+            total de los datos borrados por los usuarios, ya que los citados datos podrían haber sido suprimidos y/o modificados
+            durante el periodo del tiempo transcurrido desde la última copia de seguridad.
+        </p>
+
+        <h2 class="mt-12">Comunicaciones comerciales</h2>
+
+        <p class="text-xl">
+            En aplicación de la LSSI. Jaime Parriego Velasco no enviará comunicaciones publicitarias o promocionales por correo
+            electrónico u otro medio de comunicación electrónica equivalente que previamente no hubieran sido solicitadas o
+            expresamente autorizadas por los destinatarios de las mismas.
+        </p>
+
+        <p class="text-xl">
+            En todo caso, el usuario, tras acreditar su identidad, podrá solicitar que no se le haga llegar más información
+            comercial a través de los canales de Atención al Cliente
+        </p>
+
+    </main>
+
+    <?php require('partials/footer.php') ?>
+</body>
+
+</html>
