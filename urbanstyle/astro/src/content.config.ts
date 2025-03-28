7b20548ec415e5329c2accd4ecc0bf332908072a
alv.dev <soy@alv.dev>
2025-03-28 17:14:38 +0100

    add content config collections

---
+++ urbanstyle/astro/src/content.config.ts
@@ -0,0 +1,88 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const logos = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/sections/logos" }),
+  schema: ({ image }) =>
+    z.object({
+      logos: z.array(
+        z.object({
+          src: image(),
+          alt: z.string(),
+        }),
+      ),
+    }),
+});
+
+const reviews = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/sections/reviews" }),
+  schema: ({ image }) => 
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      enabledTitles: z.boolean(),
+      reviews: z.array(
+        z.object({
+          author: z.string(),
+          picture: image(),
+          link: z.string().url(),
+          text: z.string(),
+          stars: z.number().min(1).max(5),
+        }),
+      ),
+    })
+});
+
+const faqs = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/sections/faqs" }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      columns: z.number(),
+      description: z.string(),
+      faqs: z.array(
+        z.object({
+          question: z.string(),
+          answer: z.string(),
+        }),
+      ),
+    }),
+});
+
+const carousel = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/sections/carousel" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      description: z.string(),
+      enabledTitles: z.boolean(),
+      images: z.array(
+        z.object({
+          src: image(),
+          alt: z.string(),
+          desc: z.string(),
+        }),
+      ),
+    }),
+});
+
+const benefits = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/sections/benefits" }),
+  schema: ({ image}) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      enabledTitles: z.boolean(),
+      benefits: z.array(
+        z.object({
+          title: z.string(),
+          description: z.string(),
+          image: image(),
+          alt: z.string(),
+        }),
+      ),
+    }),
+});
+
+export const collections = { logos, reviews, faqs, carousel, benefits };
