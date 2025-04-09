8535dcf4c958bd936ee4da9e05dfdb0a29f9a467
alv.dev <soy@alv.dev>
2025-04-09 02:29:29 +0200

    add i18n initial config

--- urbanstyle/astro/astro.config.ts
+++ urbanstyle/astro/astro.config.ts
@@ -33,6 +33,14 @@ export default defineConfig({
       },
     },
   },
+  
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: false,
+    }
+  },
 
   image: {
     experimentalLayout: "responsive",
