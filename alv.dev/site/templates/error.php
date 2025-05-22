44a3bb176c916703320dadaa959a9dd7b3cc2cce
alv.dev <soy@alv.dev>
2025-05-22 06:16:50 +0200

    add 404 template

---
+++ alv.dev/site/templates/error.php
@@ -0,0 +1,7 @@
+<?php snippet('base', slots: true) ?>
+
+<?php slot() ?>
+<div class="container flex flex-col justify-center h-full">
+    <h1 class="mt-8 text-9xl font-black">Error 404</h1>
+</div>
+<?php endslot() ?>
