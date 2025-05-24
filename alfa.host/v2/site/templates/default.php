fb2d517d88c117b6d8c2c7a44af0ca43643a9c65
alv.dev <soy@alv.dev>
2025-05-24 22:29:02 +0200

    dev: add vite installation/setup

--- alfa.host/v2/site/templates/default.php
+++ alfa.host/v2/site/templates/default.php
@@ -1,1 +1,20 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?= vite()->css('assets/global.css') ?>
+</head>
+
+<body>
+    <h1><?= $page->title() ?></h1>
+    <p>This is a simple template using Vite for CSS.</p>
+
+    <div class="content">
+        <p>Content goes here...</p>
+    </div>
+</body>
+
+</html>
