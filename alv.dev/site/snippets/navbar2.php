e75a1100259b9572389bf5301b7315aa22216400
alv.dev <soy@alv.dev>
2025-05-24 20:24:44 +0200

    add dynamic latest posts + screen overlay to newsletter block

--- alv.dev/site/snippets/navbar2.php
+++ alv.dev/site/snippets/navbar2.php
@@ -3,6 +3,7 @@ $posts = ['Lorem ipsum dolor sit', 'sit amet consecttur adispicing elit', 'Lorem
 
 $linkColors = ['link', 'link-red', 'link-blue', 'link-green', 'link-yellow', 'link-violet'];
 $colorPool = $linkColors;
+$usedColors = [];
 ?>
 
 <nav class="relative text-base w-2/12">
@@ -11,26 +12,33 @@ $colorPool = $linkColors;
             <h2 class="font-bold uppercase text-xl px-8 pt-8">Últimos artículos</h2>
             <div class="scrollbar scrollbar-sm overflow-y-scroll w-full">
                 <div class="relative grid gap-6 px-8 -mt-4">
-                    <?php
-                    $usedColors = [];
-                    foreach ($posts as $i => $post) :
+                    <?php foreach ($site->find('blog')->children()->listed()->flip()->limit(5) as $key => $article) : ?>
+                        <?php
                         // Reset color pool if empty
-                        if (empty($colorPool)) {
-                            $colorPool = $linkColors;
-                        }
+                        if (empty($colorPool)) $colorPool = $linkColors;
+
                         // Pick a random color and remove it from the pool
                         $randIndex = array_rand($colorPool);
                         $linkColor = $colorPool[$randIndex];
                         unset($colorPool[$randIndex]);
-                    ?>
-                        <a class="hover:text-black hover:uppercase transition-all hover:transition-all hover:cursor-pointer">
-                            <?= $linkColor ?> <?= $post ?>
-                        </a>
-                    <?php endforeach; ?>
+                        ?>
+                        <article>
+                            <small class="text-xs flex items-center gap-1 text-slate-400 **:[svg]:w-3 **:[svg]:h-3 **:[svg]:fill-slate-500">
+                                <?= svg('assets/icons/calendar.svg') ?>
+                                <?= $article->date()->toDate('d.MM.yy') ?>
+                            </small>
+                            <a class="text-black hover:uppercase transition-all hover:transition-all hover:cursor-pointer block hyphens-auto hover:hyphens-auto" lang="es" href="<?= $article->url() ?>">
+                                <?= $article->title() ?>
+                                <span class="absolute ml-2 *:w-4.5 mt-1.5 **:fill-blue-500">
+                                    <?= svg('assets/icons/enter.svg') ?>
+                                </span>
+                            </a>
+                        </article>
+                    <?php endforeach ?>
                 </div>
             </div>
 
-            <div class="w-full p-8 bg-green-500 **:w-full">
+            <div class="screen-overlay bg-green-500 w-full p-8 **:w-full rounded-tr">
                 <h2 class="font-bold uppercase text-xl mb-4">Suscríbete</h2>
                 <div id="newsletter-message" class=" w-1/2" x-merge.transition>
                     <?php snippet('forms/newsletter') ?>
