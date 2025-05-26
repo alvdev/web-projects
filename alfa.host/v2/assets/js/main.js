860412dc0827e8c6256841d359f3d1c4631289aa
alv.dev <soy@alv.dev>
2025-05-26 18:07:04 +0200

    chore (skel): add alpinejs + plugins

--- alfa.host/v2/assets/js/main.js
+++ alfa.host/v2/assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+import Alpine from "alpinejs";
+import collapse from "@alpinejs/collapse";
+import intersect from "@alpinejs/intersect";
+
+window.Alpine = Alpine;
+Alpine.plugin(collapse);
+Alpine.plugin(intersect);
+
+Alpine.start();
