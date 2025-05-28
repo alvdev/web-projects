8f900dcbca142e6cb74fce3316ebfc3a2707aff7
alv.dev <soy@alv.dev>
2025-05-28 22:01:03 +0200

    dev: add vite + debug screens

--- alfa.host/v2/site/snippets/layouts/base.php
+++ alfa.host/v2/site/snippets/layouts/base.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?= $page->description() ?>">
     <title><?= $page->title() ?></title>
-    <?= vite()->css('assets/main.css') ?>
-    <?= vite()->js('assets/main.js') ?>
+    <?= vite()->css('assets/css/main.css') ?>
+    <?= vite()->css('assets/css/debug-screens.css') ?>
+    <?= vite()->js('assets/js/main.js') ?>
 </head>
 
-<body>
+<body class="debug-screens">
     <?php snippet('site/header') ?>
 
     <?= $slot ?>
