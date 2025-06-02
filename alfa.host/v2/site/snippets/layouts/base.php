d2da2dbd4f7a7292f68f22e8b2f53a9dd33da893
alv.dev <soy@alv.dev>
2025-06-02 19:46:37 +0200

    fix: debug screen overwriting global styles

--- alfa.host/v2/site/snippets/layouts/base.php
+++ alfa.host/v2/site/snippets/layouts/base.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?= $page->description() ?>">
     <title><?= $page->title() ?></title>
-    <?= vite()->css('assets/css/main.css') ?>
     <?= vite()->css('assets/css/debug-screens.css', try: true) ?>
+    <?= vite()->css('assets/css/main.css') ?>
     <?= vite()->js('assets/js/main.js') ?>
 </head>
 
