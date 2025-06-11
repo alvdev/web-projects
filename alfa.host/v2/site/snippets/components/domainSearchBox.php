59d75add8b755cb90e65ab1313dd99cc34d2f031
alv.dev <soy@alv.dev>
2025-06-11 06:45:07 +0200

    style: improve domain search box

--- alfa.host/v2/site/snippets/components/domainSearchBox.php
+++ alfa.host/v2/site/snippets/components/domainSearchBox.php
@@ -1,5 +1,5 @@
-<div class="p-2 bg-linear-to-b from-white/30 to-white/50 rounded-xl backdrop-blur-sm">
-    <div class="flex flex-col items-center relative p-12 bg-linear-to-b from-slate-300 to-white/80 rounded-lg lg:flex-row gap-x-8">
+<div class="p-2 bg-linear-to-b from-white/30 to-white/50 rounded-xl backdrop-blur-sm shadow-xl shadow-slate-300/50">
+    <div class="flex flex-col items-center relative p-12 bg-linear-to-b from-blue-100/80 to-white/80 rounded-lg lg:flex-row gap-x-8">
         <h3 class="relative text-balance text-3xl font-normal md:w-full lg:w-2/5">
             Todo emprendedor digital necesita un dominio donde crear su negocio online
             <span class="hidden md:block md:absolute md:-right-16 md:top-4 md:z-0 md:w-32">
@@ -10,7 +10,7 @@
 
         <div class="w-full lg:w-3/5">
             <form class="" action="./">
-                <div class="bg-white border-4 rounded-full overflow-clip flex flex-col justify-around w-full sm:flex-row">
+                <div class="ring-1 ring-black/5 bg-white border-4 rounded-full overflow-clip flex flex-col justify-around w-full sm:flex-row">
                     <input class="w-full bg-white text-black font-medium px-6 py-4 text-2xl uppercase focus:outline-hidden placeholder:text-black focus:placeholder:text-black/20" type="text" placeholder="elnombredetunegocio.com" spellcheck="false" data-ms-editor="true">
 
                     <button class="w-full p-4 text-white uppercase bg-black rounded-full shadow-md hover:shadow-inner">
