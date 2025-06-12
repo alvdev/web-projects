28aff73f9b9f27610b025e2d6f8a7b0685c5faaf
alv.dev <soy@alv.dev>
2025-06-12 14:46:49 +0200

    feat: add scaffold error page

---
+++ alfa.host/v2/site/templates/error.php
@@ -0,0 +1,7 @@
+<?php snippet('layouts/base', slots: true) ?>
+
+<?php slot() ?>
+<main class="container mt-48 flex flex-col justify-center h-full">
+    <h1 class="mt-8 text-9xl font-black">Error 404</h1>
+</main>
+<?php endslot() ?>
