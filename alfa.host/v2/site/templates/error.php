2a7296fec523c2c45d72dc9580db488116181a76
alv.dev <soy@alv.dev>
2025-06-16 18:31:53 +0200

    style: add error pages enhancements

--- alfa.host/v2/site/templates/error.php
+++ alfa.host/v2/site/templates/error.php
@@ -1,7 +1,11 @@
 <?php snippet('layouts/base', slots: true) ?>
 
 <?php slot() ?>
-<main class="container mt-48 flex flex-col justify-center h-full">
-    <h1 class="mt-8 text-9xl font-black">Error 404</h1>
+
+<main class="pt-56 pb-24">
+    <section class="container">
+        <h1 class="mt-8 text-9xl">Error 404</h1>
+    </section>
 </main>
+
 <?php endslot() ?>
