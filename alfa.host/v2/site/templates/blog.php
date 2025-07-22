cfbe7eff6c2b7a3058a09fe5427a4c299766db28
alv.dev <soy@alv.dev>
2025-07-22 19:35:07 +0200

    (blog): add featured articles section

--- alfa.host/v2/site/templates/blog.php
+++ alfa.host/v2/site/templates/blog.php
@@ -3,10 +3,29 @@
 <main class="pt-48">
     <?php snippet('sections/heroBlog') ?>
 
-    <section class="main-content">
+    <section class="pt-16 container bg-ellipse-t-blue">
+        <div class="relative -mb-8 z-10 grid grid-cols-3 gap-1 p-1 bg-linear-to-b from-blue-300/90 to-white/50 rounded-xl backdrop-blur-sm shadow-slate-300/50 shadow-sm">
+            <article class="relative rounded-lg overflow-clip">
+                <img src="https://picsum.photos/800/500?random=1" class="aspect-video" alt="">
+                <h2 class="text-gradient-none text-white text-shadow-xs absolute bottom-0 z-0 w-full text-3xl font-semibold px-4 pt-8 pb-3 after:absolute after:left-0 after:bottom-0 after:-z-10 after:w-full after:h-full after:bg-gradient-to-t after:from-black/80 after:to-transparent">Lorem ipsum</h2>
+            </article>
+
+            <article class="relative rounded-lg overflow-clip">
+                <img src="https://picsum.photos/800/500?random=2" class="aspect-video" alt="">
+                <h2 class="text-gradient-none text-white text-shadow-xs absolute bottom-0 z-0 w-full text-3xl font-semibold px-4 pt-8 pb-3 after:absolute after:left-0 after:bottom-0 after:-z-10 after:w-full after:h-full after:bg-gradient-to-t after:from-black/80 after:to-transparent">Lorem ipsum</h2>
+            </article>
+
+            <article class="relative rounded-lg overflow-clip">
+                <img src="https://picsum.photos/800/500?random=3" class="aspect-video" alt="">
+                <h2 class="text-gradient-none text-white text-shadow-xs absolute bottom-0 z-0 w-full text-3xl font-semibold px-4 pt-8 pb-3 after:absolute after:left-0 after:bottom-0 after:-z-10 after:w-full after:h-full after:bg-gradient-to-t after:from-black/80 after:to-transparent">Lorem ipsum</h2>
+            </article>
+        </div>
+    </section>
+
+    <section class="main-content"> 
         <div class="container mt-16">
             <div class="border-2 border-red-500">
-                <?= $page->categories() ?>xxxx
+                <?= $page->categories() ?>
             </div>
             <div class="border-2 border-blue-500">
                 <?= $site->children()->template('blog')->categories() ?>
