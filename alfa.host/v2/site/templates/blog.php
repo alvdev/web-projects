3e32660a7092b8b32b72ecba0746e5a1658306a9
alv.dev <soy@alv.dev>
2025-07-22 20:23:17 +0200

    (blog): add categories aside placeholder

--- alfa.host/v2/site/templates/blog.php
+++ alfa.host/v2/site/templates/blog.php
@@ -23,9 +23,9 @@
     </section>
 
     <section class="main-content">
-        <div class="container flex gap-8">
+        <div class="container mt-24 flex gap-8">
             <div class="w-2/3">
-                <section id="latest-posts" class="container mt-24 grid grid-cols-2 gap-4">
+                <section id="latest-posts" class="container grid grid-cols-2 gap-4">
                     <article>
                         <img src="https://picsum.photos/600/400?random=4" class="aspect-video rounded-lg" alt="">
                         <h2 class="mt-6 text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
@@ -39,7 +39,54 @@
                     </article>
                 </section>
             </div>
+
+            <aside id="aside-right" class="self-start sticky top-24 flex flex-col gap-8">
+                <div>
+                    <h2 class="border-l-4 border-blue-600 mb-1 pl-4 py-2 uppercase text-xl">Productos y servicios</h2>
+                    <ul class="border-l-4 border-gray-200 mb-1 pl-4 py-2 grid grid-cols-2 gap-4">
+                        <li class="text-blue-600">Web hosting</li>
+                        <li class="text-blue-600">Servidores</li>
+                        <li class="text-blue-600">Marketing</li>
+                        <li class="text-blue-600">Alfa mailer</li>
+                        <li class="text-blue-600">Alfa CMS</li>
+                        <li class="text-blue-600">Alfa Page</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 class="border-l-4 border-blue-600 mb-1 pl-4 py-2 uppercase text-xl">Gestores de contenido</h2>
+                    <ul class="border-l-4 border-gray-200 mb-1 pl-4 py-2 grid grid-cols-2 gap-4">
+                        <li class="text-blue-600">Drupal</li>
+                        <li class="text-blue-600">Wordpress</li>
+                        <li class="text-blue-600">Moodle</li>
+                        <li class="text-blue-600">Prestashop</li>
+                        <li class="text-blue-600">Magento</li>
+                        <li class="text-blue-600">WooCommerce</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 class="border-l-4 border-blue-600 mb-1 pl-4 py-2 uppercase text-xl">Negocios digitales</h2>
+                    <ul class="border-l-4 border-gray-200 mb-1 pl-4 py-2 grid grid-cols-2 gap-4">
+                        <li class="text-blue-600">Portales verticales</li>
+                        <li class="text-blue-600">Programas de afiliados</li>
+                        <li class="text-blue-600">Comercio electrónico</li>
+                        <li class="text-blue-600">Servicios por suscripción</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 class="border-l-4 border-blue-600 mb-1 pl-4 py-2 uppercase text-xl">Marketing en internet</h2>
+                    <ul class="border-l-4 border-gray-200 mb-1 pl-4 py-2 grid grid-cols-2 gap-4">
+                        <li class="text-blue-600">Email marketing</li>
+                        <li class="text-blue-600">Posicionamiento web</li>
+                        <li class="text-blue-600">Social Media Marketing</li>
+                        <li class="text-blue-600">Creación de contenidos</li>
+                    </ul>
+                </div>
+            </aside>
         </div>
+
         <div class="container mt-16">
             <div class="border-2 border-red-500">
                 <?= $page->categories() ?>
