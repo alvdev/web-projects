c770c950933408117400d5d8bc67bdb0c7de02dc
alv.dev <soy@alv.dev>
2025-12-03 11:21:42 +0100

    feat: add ntro section

---
+++ magoscott/site/snippets/sections/intro.php
@@ -0,0 +1,19 @@
+<section id="intro" class="<?= $class ?>">
+    <div class="container">
+        <h2 class="text-6xl">¿Quién es el Mago Scott?</h2>
+
+        <div class="mt-8 flex items-center gap-32">
+            <div class="mt-8 *:text-3xl *:not-first:mt-8 **:[span]:text-violet-300 **:[span]:font-semibold">
+                <p>El Mago Scott es un <span>artista polifacético</span>, más conocido como el Mago de tele.</p>
+                <p>Además de sus espectáculos de magia y humor, también ha realizado trabajos como <span>presentador</span> en eventos, <span>actor de doblaje</span> para anuncios, <span>técnico de efectos especiales</span> para teatros, musicales y conciertos.</p>
+                <p>También ha sido galardonado con el <span>premio al mejor Showman internacional</span> en el festival de San Sebastián en 2007.</p>
+                <p>Actualmente, el Mago Scott colabora con diferentes programas de Telecinco.</p>
+            </div>
+
+            <div class="flex flex-col gap-8 text-2xl *:border *:whitespace-nowrap *:text-center *:rounded-full *:px-8 *:pt-4 *:pb-4.75">
+                <a href="#">Contratación</a>
+                <a href="#">Comprar entradas</a>
+            </div>
+        </div>
+    </div>
+</section>
