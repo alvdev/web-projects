1eca6841adf1e90d82f3aa4bda1a0fc161a75c65
alv.dev <soy@alv.dev>
2025-12-07 23:53:41 +0100

    feat (panel): add gallery menu + default srcsets

--- magoscott/site/config/config.php
+++ magoscott/site/config/config.php
@@ -35,11 +35,20 @@ return [
             ],
             'blog' => [
                 'label' => 'Blog',
-                'icon' => 'blog',
+                'icon' => 'document',
                 'link' => 'pages/blog',
                 'current' => function (): bool {
                     $path = App::instance()->path();
                     return Str::contains($path, 'pages/blog');
+                }
+            ],
+            'pictures' => [
+                'label' => 'Galería',
+                'icon' => 'images',
+                'link' => 'pages/gallery',
+                'current' => function (): bool {
+                    $path = App::instance()->path();
+                    return Str::contains($path, 'pages/gallery');
                 }
             ],
             '-',
@@ -88,5 +97,30 @@ return [
                 }
             }
         ]
-    ]
+    ],
+    'thumbs' => [
+        'srcsets' => [
+            'default' => [
+                '300w'  => ['width' => 300],
+                '600w'  => ['width' => 600],
+                '900w'  => ['width' => 900],
+                '1200w' => ['width' => 1200],
+                '1800w' => ['width' => 1800]
+            ],
+            'avif' => [
+                '300w'  => ['width' => 300, 'format' => 'avif'],
+                '600w'  => ['width' => 600, 'format' => 'avif'],
+                '900w'  => ['width' => 900, 'format' => 'avif'],
+                '1200w' => ['width' => 1200, 'format' => 'avif'],
+                '1800w' => ['width' => 1800, 'format' => 'avif']
+            ],
+            'webp' => [
+                '300w'  => ['width' => 300, 'format' => 'webp'],
+                '600w'  => ['width' => 600, 'format' => 'webp'],
+                '900w'  => ['width' => 900, 'format' => 'webp'],
+                '1200w' => ['width' => 1200, 'format' => 'webp'],
+                '1800w' => ['width' => 1800, 'format' => 'webp']
+            ],
+        ]
+    ],
 ];
