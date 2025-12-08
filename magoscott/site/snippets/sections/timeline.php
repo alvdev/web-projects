5ed641b6d852a027b950c8df6e996363a04ca48b
alv.dev <soy@alv.dev>
2025-12-08 03:08:13 +0100

    feat: add timeline snippet

--- magoscott/site/snippets/sections/timeline.php
+++ magoscott/site/snippets/sections/timeline.php
@@ -1,162 +1,63 @@
-<div class="container">
-    <div class="relative lg:ml-[max(calc(9.5rem+1px),calc(100%-80rem))]">
-        <div
-            class="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-zinc-200 lg:block"></div>
-        <div class="mt-12 space-y-8">
-            <article class="relative group">
-                <div class="relative hidden mr-10 lg:flex md:mx-auto">
-                    <div class="flex items-center justify-center w-3 h-full">
-                        <div class="h-full w-[0.05rem] bg-zinc-200 pointer-events-none"></div>
-                    </div>
-                    <div
-                        class="absolute right-full mr-6 top-2 text-blue-500 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
-                        <div
-                            class="rounded-full size-full outline-2 outline-zinc-100 bg-zinc-100"></div>
-                    </div>
-                </div>
-                <div
-                    class="relative flex flex-col p-4 shadow bg-linear-180 outline outline-zinc-100 from-zinc-50 to-zinc-100 rounded-xl">
-                    <h3
-                        class="text-lg md:text-xl mt-4 font-semibold tracking-tight lg:mt-0 text-zinc-900">
-                        Introducing Version 3.0.0
-                    </h3>
-                    <p class="text-base mt-2 text-zinc-500 lg:text-balance">
-                        Kickstart a new era with a refreshed interface, AI-driven tools, and
-                        seamless app integrations to boost your productivity.
-                    </p>
-                    <div
-                        class="lg:absolute order-first lg:top-0 tracking-tight text-sm text-zinc-500 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
-                        <p
-                            class="text-xs relative font-semibold uppercase tracking-wide text-zinc-500">
-                            <span class="sr-only">Release date</span>
-                            <time datetime="2025-05-21">May 21</time>
-                        </p>
-                    </div>
-                </div>
-            </article>
-            <article class="relative group">
-                <div class="relative hidden mr-10 lg:flex md:mx-auto">
-                    <div class="flex items-center justify-center w-3 h-full">
-                        <div class="h-full w-[0.05rem] bg-zinc-200 pointer-events-none"></div>
-                    </div>
-                    <div
-                        class="absolute right-full mr-6 top-2 text-blue-500 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
-                        <div
-                            class="rounded-full size-full outline-2 outline-zinc-100 bg-zinc-100"></div>
-                    </div>
-                </div>
-                <div
-                    class="relative flex flex-col p-4 shadow bg-linear-180 outline outline-zinc-100 from-zinc-50 to-zinc-100 rounded-xl">
-                    <h3
-                        class="text-lg md:text-xl mt-4 font-semibold tracking-tight lg:mt-0 text-zinc-900">
-                        Update 3.1.0: Stellar Performance
-                    </h3>
-                    <p class="text-base mt-2 text-zinc-500 lg:text-balance">
-                        Enhance your workflow with faster processing, deeper customization,
-                        and vibrant new visuals for an out-of-this-world experience.
-                    </p>
-                    <div
-                        class="lg:absolute lg:left-0 order-first lg:top-0 tracking-tight text-sm text-zinc-500 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
-                        <p
-                            class="text-xs relative font-semibold uppercase tracking-wide text-zinc-500">
-                            <span class="sr-only">Release date</span>
-                            <time datetime="2025-06-15">Jun 15</time>
-                        </p>
-                    </div>
-                </div>
-            </article>
-            <article class="relative group">
-                <div class="relative hidden mr-10 lg:flex md:mx-auto">
-                    <div class="flex items-center justify-center w-3 h-full">
-                        <div class="h-full w-[0.05rem] bg-zinc-200 pointer-events-none"></div>
-                    </div>
-                    <div
-                        class="absolute right-full mr-6 top-2 text-blue-500 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
-                        <div
-                            class="rounded-full size-full outline-2 outline-zinc-100 bg-zinc-100"></div>
-                    </div>
-                </div>
-                <div
-                    class="relative flex flex-col p-4 shadow bg-linear-180 outline outline-zinc-100 from-zinc-50 to-zinc-100 rounded-xl">
-                    <h3
-                        class="text-lg md:text-xl mt-4 font-semibold tracking-tight lg:mt-0 text-zinc-900">
-                        Release 3.2.0: AI Onboarding
-                    </h3>
-                    <p class="text-base mt-2 text-zinc-500 lg:text-balance">
-                        Streamline AI setup with intuitive wizards and quick-start templates
-                        to get you up and running fast.
-                    </p>
-                    <div
-                        class="lg:absolute lg:left-0 order-first lg:top-0 tracking-tight text-sm text-zinc-500 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
-                        <p
-                            class="text-xs relative font-semibold uppercase tracking-wide text-zinc-500">
-                            <span class="sr-only">Release date</span>
-                            <time datetime="2025-07-30">Jul 30</time>
-                        </p>
-                    </div>
-                </div>
-            </article>
-            <article class="relative group">
-                <div class="relative hidden mr-10 lg:flex md:mx-auto">
-                    <div class="flex items-center justify-center w-3 h-full">
-                        <div class="h-full w-[0.05rem] bg-zinc-200 pointer-events-none"></div>
-                    </div>
-                    <div
-                        class="absolute right-full mr-6 top-2 text-blue-500 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
-                        <div
-                            class="rounded-full size-full outline-2 outline-zinc-100 bg-zinc-100"></div>
-                    </div>
-                </div>
-                <div
-                    class="relative flex flex-col p-4 shadow bg-linear-180 outline outline-zinc-100 from-zinc-50 to-zinc-100 rounded-xl">
-                    <h3
-                        class="text-lg md:text-xl mt-4 font-semibold tracking-tight lg:mt-0 text-zinc-900">
-                        Introducing Version 4.0: Next-Gen Insights
-                    </h3>
-                    <p class="text-base mt-2 text-zinc-500 lg:text-balance">
-                        Unlock real-time analytics and predictive insights with our most
-                        powerful update yet.
-                    </p>
-                    <div
-                        class="lg:absolute lg:left-0 order-first lg:top-0 tracking-tight text-sm text-zinc-500 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
-                        <p
-                            class="text-xs relative font-semibold uppercase tracking-wide text-zinc-500">
-                            <span class="sr-only">Release date</span>
-                            <time datetime="2025-09-10">Sep 10</time>
-                        </p>
-                    </div>
-                </div>
-            </article>
-            <article class="relative group">
-                <div class="relative hidden mr-10 lg:flex md:mx-auto">
-                    <div class="flex items-center justify-center w-3 h-full">
-                        <div class="h-full w-[0.05rem] bg-zinc-200 pointer-events-none"></div>
-                    </div>
-                    <div
-                        class="absolute right-full mr-6 top-2 text-blue-500 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
-                        <div
-                            class="rounded-full size-full outline-2 outline-zinc-100 bg-zinc-100"></div>
-                    </div>
-                </div>
-                <div
-                    class="relative flex flex-col p-4 shadow bg-linear-180 outline outline-zinc-100 from-zinc-50 to-zinc-100 rounded-xl">
-                    <h3
-                        class="text-lg md:text-xl mt-4 font-semibold tracking-tight lg:mt-0 text-zinc-900">
-                        Patch 4.1: Performance Boost
-                    </h3>
-                    <p class="text-base mt-2 text-zinc-500 lg:text-balance">
-                        Enhancements for speed, stability, and cross-platform compatibility.
-                    </p>
-                    <div
-                        class="lg:absolute lg:left-0 order-first lg:top-0 tracking-tight text-sm text-zinc-500 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
-                        <p
-                            class="text-xs relative font-semibold uppercase tracking-wide text-zinc-500">
-                            <span class="sr-only">Release date</span>
-                            <time datetime="2025-10-05">Oct 05</time>
-                        </p>
-                    </div>
-                </div>
-            </article>
+<section class="bg-linear-to-bl from-red-600/30 to-indigo-950/30 to-50% <?= $class ?>">
+    <div class="container">
+        <div class="relative lg:ml-[max(calc(9.5rem+1px),calc(100%-80rem))]">
+            <div
+                class="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-13 w-px bg-zinc-200 lg:block"></div>
+            <div class="mt-36 *:not-first:mt-16">
+
+                <?php if ($page->hasChildren()): {
+                        $items = $page->children()->listed()->sortBy('yearTo', 'desc', 'yearFrom', 'desc');
+                    } ?>
+                    <?php foreach ($items as $item): ?>
+                        <article class="relative group">
+                            <div class="relative hidden mr-10 lg:flex md:mx-auto">
+                                <div class="flex items-center justify-center w-3 h-full">
+                                    <div class="h-full w-[0.05rem] bg-zinc-200 pointer-events-none"></div>
+                                </div>
+                                <div class="absolute right-full mr-6 top-2 text-blue-500 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block">
+                                    <div
+                                        class="rounded-full size-full outline-2 outline-violet-100 bg-zinc-100"></div>
+                                </div>
+                            </div>
+
+                            <div class="relative -top-2 flex justify-between gap-24">
+                                <div>
+                                    <h3 class="relative text-xl md:text-2xl mt-4 font-semibold lg:mt-0 first-letter:uppercase after:absolute after:-bottom-4 after:left-0 after:bg-red-600 after:w-36 after:h-1 after:rounded-full">
+                                        <?= $item->title() ?>
+                                    </h3>
+    
+                                    <div class="mt-8 text-pretty text-white/80">
+                                        <?= $item->description()->kt() ?>
+                                    </div>
+                                </div>
+
+                                <?php if ($item->content()->videos()->isNotEmpty()): ?>
+                                    <div class="flex gap-8 *:rounded-xl *:ring-2 *:ring-indigo-900/50 *:aspect-video">
+                                        <?php foreach ($item->content()->videos()->toEntries() as $entry): ?>
+                                            <?= youtube($entry) ?>
+                                        <?php endforeach ?>
+                                    </div>
+                                <?php endif ?>
+
+                                <div class="lg:absolute order-first lg:top-0 tracking-tight text-lg lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
+                                    <p class="relative text-2xl font-semibold uppercase tracking-wide text-violet-300">
+                                        <span class="sr-only">Año</span>
+                                        <time class="leading-none">
+                                            <?php if ($item->yearFrom()->isNotEmpty() && $item->yearTo()->isNotEmpty()): ?>
+                                                <?= $item->yearTo() ?>
+                                                <span class="relative -top-2 block text-center">↑<span>
+                                                        <?= $item->yearFrom() ?>
+                                                    <?php else: ?>
+                                                        <?= $item->yearFrom() ?>
+                                                    <?php endif ?>
+                                        </time>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    <?php endforeach ?>
+                <?php endif ?>
+            </div>
         </div>
     </div>
-</div>
+</section>
