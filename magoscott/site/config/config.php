58b19af76218a25548df86c8d4a320ef3c58a347
alv.dev <soy@alv.dev>
2025-12-10 15:52:00 +0100

    feat: add dotenv

--- magoscott/site/config/config.php
+++ magoscott/site/config/config.php
@@ -13,114 +13,124 @@ use Kirby\Toolkit\A;
 use Kirby\Toolkit\Str;
 
 return [
-    'debug' => true,
-    'fatal' => function ($kirby, $exception) {
-        include $kirby->root('templates') . '/fatal.php';
-    },
-    'date.handler' => 'intl',
-    'author.seo-audit' => [
-        'option' => 'alv dev'
-    ],
-    'languages' => true,
-    'panel' => [
-        'favicon' => 'assets/favicon.png',
-        'menu' => [
-            'site' => [
-                'current' => function (): bool {
-                    $links = ['site'];
+    // alv.dev: Implement ready function because of dotenv
+    'ready' => function () {
+        return [
+            'debug' => env('APP_DEBUG', true),
+            'fatal' => function ($kirby, $exception) {
+                include $kirby->root('templates') . '/fatal.php';
+            },
+            'date.handler' => 'intl',
+            'author.seo-audit' => [
+                'option' => 'alv dev'
+            ],
+            'languages' => true,
+            'panel' => [
+                'favicon' => 'assets/favicon.png',
+                'menu' => [
+                    'site' => [
+                        'current' => function (): bool {
+                            $links = ['site'];
 
-                    $path  = App::instance()->path();
-                    return A::every($links, fn($link) => Str::contains($path, $link));
-                },
+                            $path  = App::instance()->path();
+                            return A::every($links, fn($link) => Str::contains($path, $link));
+                        },
+                    ],
+                    'blog' => [
+                        'label' => 'Blog',
+                        'icon' => 'document',
+                        'link' => 'pages/blog',
+                        'current' => function (): bool {
+                            $path = App::instance()->path();
+                            return Str::contains($path, 'pages/blog');
+                        }
+                    ],
+                    'pictures' => [
+                        'label' => 'Galería',
+                        'icon' => 'images',
+                        'link' => 'pages/gallery',
+                        'current' => function (): bool {
+                            $path = App::instance()->path();
+                            return Str::contains($path, 'pages/gallery');
+                        }
+                    ],
+                    '-',
+                    'users',
+                    'forms' => [
+                        'label' => 'Forms',
+                        'icon' => 'form',
+                        'link' => 'pages/forms',
+                        'current' => function (): bool {
+                            $path = App::instance()->path();
+                            return Str::contains($path, 'pages/forms');
+                        }
+                    ],
+                    'languages',
+                    'system',
+                ]
             ],
-            'blog' => [
-                'label' => 'Blog',
-                'icon' => 'document',
-                'link' => 'pages/blog',
-                'current' => function (): bool {
-                    $path = App::instance()->path();
-                    return Str::contains($path, 'pages/blog');
-                }
+            'mauricerenck.PexelsImageField' => [
+                'apiKey' => env('PEXELS_APIKEY'),
+                'downloadSize' => 'landscape',
+                'images' => 7
             ],
-            'pictures' => [
-                'label' => 'Galería',
-                'icon' => 'images',
-                'link' => 'pages/gallery',
-                'current' => function (): bool {
-                    $path = App::instance()->path();
-                    return Str::contains($path, 'pages/gallery');
-                }
+            'tobimori.dreamform' => [
+                'guards' => [
+                    'available' => ['csrf', 'honeypot', 'turnstile', /* 'ratelimit' */],
+                    'honeypot.availableFields' => ['website', 'email', 'name', 'url', 'birthdate'],
+                ],
             ],
-            '-',
-            'users',
-            'forms' => [
-                'label' => 'Forms',
-                'icon' => 'form',
-                'link' => 'pages/forms',
-                'current' => function (): bool {
-                    $path = App::instance()->path();
-                    return Str::contains($path, 'pages/forms');
-                }
+            'routes' => [
+                [
+                    'pattern' => 'blog/(:any)',
+                    'action' => function ($cat) {
+                        if ($page = page('blog/' . $cat)) {
+                            return $page;
+                        } else {
+                            return page('blog')->render([
+                                'cat' => $cat
+                            ]);
+                        }
+                    }
+                ],
+                [
+                    'pattern' => 'blog/tag/(:any)',
+                    'action' => function ($tag) {
+                        if ($page = page('blog/' . $tag)) {
+                            return $page;
+                        } else {
+                            return page('blog')->render([
+                                'tag' => $tag
+                            ]);
+                        }
+                    }
+                ]
             ],
-            'languages',
-            'system',
-        ]
-    ],
-    'tobimori.dreamform' => [
-        'guards' => [
-            'available' => ['csrf', 'honeypot', 'turnstile', /* 'ratelimit' */],
-            'honeypot.availableFields' => ['website', 'email', 'name', 'url', 'birthdate'],
-        ],
-    ],
-    'routes' => [
-        [
-            'pattern' => 'blog/(:any)',
-            'action' => function ($cat) {
-                if ($page = page('blog/' . $cat)) {
-                    return $page;
-                } else {
-                    return page('blog')->render([
-                        'cat' => $cat
-                    ]);
-                }
-            }
-        ],
-        [
-            'pattern' => 'blog/tag/(:any)',
-            'action' => function ($tag) {
-                if ($page = page('blog/' . $tag)) {
-                    return $page;
-                } else {
-                    return page('blog')->render([
-                        'tag' => $tag
-                    ]);
-                }
-            }
-        ]
-    ],
-    'thumbs' => [
-        'srcsets' => [
-            'default' => [
-                '300w'  => ['width' => 300],
-                '600w'  => ['width' => 600],
-                '900w'  => ['width' => 900],
-                '1200w' => ['width' => 1200],
-                '1800w' => ['width' => 1800]
+            'thumbs' => [
+                'srcsets' => [
+                    'default' => [
+                        '300w'  => ['width' => 300],
+                        '600w'  => ['width' => 600],
+                        '900w'  => ['width' => 900],
+                        '1200w' => ['width' => 1200],
+                        '1800w' => ['width' => 1800]
+                    ],
+                    'avif' => [
+                        '300w'  => ['width' => 300, 'format' => 'avif'],
+                        '600w'  => ['width' => 600, 'format' => 'avif'],
+                        '900w'  => ['width' => 900, 'format' => 'avif'],
+                        '1200w' => ['width' => 1200, 'format' => 'avif'],
+                        '1800w' => ['width' => 1800, 'format' => 'avif']
+                    ],
+                    'webp' => [
+                        '300w'  => ['width' => 300, 'format' => 'webp'],
+                        '600w'  => ['width' => 600, 'format' => 'webp'],
+                        '900w'  => ['width' => 900, 'format' => 'webp'],
+                        '1200w' => ['width' => 1200, 'format' => 'webp'],
+                        '1800w' => ['width' => 1800, 'format' => 'webp']
+                    ],
+                ]
             ],
-            'avif' => [
-                '300w'  => ['width' => 300, 'format' => 'avif'],
-                '600w'  => ['width' => 600, 'format' => 'avif'],
-                '900w'  => ['width' => 900, 'format' => 'avif'],
-                '1200w' => ['width' => 1200, 'format' => 'avif'],
-                '1800w' => ['width' => 1800, 'format' => 'avif']
-            ],
-            'webp' => [
-                '300w'  => ['width' => 300, 'format' => 'webp'],
-                '600w'  => ['width' => 600, 'format' => 'webp'],
-                '900w'  => ['width' => 900, 'format' => 'webp'],
-                '1200w' => ['width' => 1200, 'format' => 'webp'],
-                '1800w' => ['width' => 1800, 'format' => 'webp']
-            ],
-        ]
-    ],
+        ];
+    }
 ];
