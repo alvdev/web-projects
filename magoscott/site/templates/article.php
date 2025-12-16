cfb93bb68864a2c822759bb438039b790ce16396
alv.dev <soy@alv.dev>
2025-12-16 20:44:22 +0100

    feat: add article template

---
+++ magoscott/site/templates/article.php
@@ -0,0 +1,54 @@
+<?php snippet('base', slots: true) ?>
+<?php slot('default') ?>
+
+<?php
+$sizes = "
+        (min-width: 1200px) 25vw,
+        (min-width: 900px) 33vw,
+        (min-width: 600px) 50vw,
+        100vw";
+?>
+
+<main class="bg-linear-to-tl from-indigo-950 via-indigo-950 via-50% to-black">
+    <article class="container w-3/5 pt-64">
+        <div class="relative pl-8 before:absolute before:left-0 before:w-1 before:h-full before:bg-red-600 before:rounded-full">
+            <div class="absolute -left-[50px] -top-4 *:border-4 *:border-red-600 *:rounded-full *:shadow-[0_0_50px_rgb(255_255_255/30%)]">
+                <?= asset('assets/images/scott-face.webp')->crop(100, 100) ?>
+            </div>
+
+            <h1 class="pl-12 text-7xl font-bold text-shadow-lg leading-none text-balance first-letter:uppercase">
+                <?= $page->title() ?>
+            </h1>
+
+            <?php if ($summary = $page->summary()): ?>
+                <p class="mt-8 text-3xl text-white/80"><?= $summary ?></p>
+            <?php endif ?>
+        </div>
+
+        <?php if ($image = $page->cover()->toFile()): ?>
+            <picture>
+                <source
+                    srcset="<?= $image->srcset('avif') ?>"
+                    sizes="<?= $sizes ?>"
+                    type="image/avif">
+                <source
+                    srcset="<?= $image->srcset('webp') ?>"
+                    sizes="<?= $sizes ?>"
+                    type="image/webp">
+                <img class="mt-16 rounded-2xl ring-4 ring-violet-500/20 aspect-video object-cover w-full bg-violet-950"
+                    alt="<?= $image->alt() ?>"
+                    src="<?= $image->resize(300)->url() ?>"
+                    srcset="<?= $image->srcset() ?>"
+                    sizes="<?= $sizes ?>"
+                    width="<?= $image->resize(1800)->width() ?>"
+                    height="<?= $image->resize(1800)->height() ?>">
+            </picture>
+        <?php endif ?>
+
+        <div class="relative mt-16 prose-2xl pl-8 text-white/90 before:absolute before:left-0 before:w-1 before:h-full before:bg-red-600 before:rounded-full">
+            <?= $page->text()->kt() ?>
+        </div>
+    </article>
+</main>
+
+<?php endslot() ?>
