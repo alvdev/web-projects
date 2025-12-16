8827450bb294b7c8185b20c0aa24e265cbdfab84
alv.dev <soy@alv.dev>
2025-12-16 20:42:20 +0100

    feat: add home header section

---
+++ magoscott/site/snippets/sections/headers/home.php
@@ -0,0 +1,16 @@
+<section id="home-header" class="bg-linear-to-tl from-red-600 via-indigo-950 via-50% to-black">
+    <div class="container pt-64 flex items-center gap-32 *:w-1/2">
+        <div>
+            <hgroup>
+                <h1 class="text-7xl font-bold leading-none text-balance">Ofrece una experiencia única</h1>
+                <p class="mt-8 text-4xl text-pretty">Puedes tener al Mago Scott en tu pub, discoteca o fiesta privada para que sientas la magia y el humor en directo.</p>
+            </hgroup>
+
+            <?php snippet('forms/contact') ?>
+        </div>
+
+        <div>
+            <?= asset('assets/images/headers/scott.png') ?>
+        </div>
+    </div>
+</section>
