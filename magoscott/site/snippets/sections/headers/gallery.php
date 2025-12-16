0449148b8c2ba9945137af21724e989b012bdab6
alv.dev <soy@alv.dev>
2025-12-16 20:42:36 +0100

    feat: add gallery header section

---
+++ magoscott/site/snippets/sections/headers/gallery.php
@@ -0,0 +1,16 @@
+<header id="home-header" class="bg-linear-to-tl from-red-600/30 via-indigo-950/30 via-50% to-black/30">
+    <div class="absolute top-0 -z-10 opacity-90 *:mask-b-to-90%">
+       <?= $page->cover()->toFile() ?> 
+    </div>
+
+    <div class="container relative pt-64">
+        <hgroup class="text-center text-balance max-w-2/3 mx-auto text-shadow-sm text-shadow-black">
+            <h1 class="text-7xl font-bold leading-none text-balance">
+                <?= $page->headerTitle() ?>
+            </h1>
+            <p class="mt-8 text-4xl text-pretty">
+                <?= $page->description() ?>
+            </p>
+        </hgroup>
+    </div>
+</header>
