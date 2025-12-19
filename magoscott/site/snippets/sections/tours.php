21ee49f54bc3d71ee0771695d899a979c826a162
alv.dev <soy@alv.dev>
2025-12-19 20:44:03 +0100

    (home tours): add responsive design

--- magoscott/site/snippets/sections/tours.php
+++ magoscott/site/snippets/sections/tours.php
@@ -1,18 +1,18 @@
 <section id="tours" class="<?= $class ?>">
     <div class="container">
-        <div class="flex items-baseline gap-8">
-        <h2 class="text-6xl whitespace-nowrap"><?= $site->tourTitle() ?></h2>
-            <div class="w-full h-1 bg-red-500 rounded-full"></div>
+        <div class="flex items-end gap-8">
+            <h2 class="text-4xl md:text-6xl whitespace-nowrap"><?= $site->tourTitle() ?></h2>
+            <div class="w-full h-1 bg-red-500 rounded-full mb-3"></div>
         </div>
 
-        <div class="mt-16 flex items-center gap-18 *:w-1/2">
-            <div class="*:w-full *:h-auto *:aspect-video *:rounded-2xl *:border-2 *:border-violet-900/30">
+        <div class="mt-12 md:mt-16 flex flex-col lg:flex-row items-center gap-8 md:gap-12 lg:gap-18 lg:*:w-1/2">
+            <div class="w-full *:w-full *:h-auto *:aspect-video *:rounded-2xl *:border-2 *:border-violet-900/30">
                 <?= $site->tourVideo()->toEmbed()->code() ?>
             </div>
 
-            <div class="*:text-2xl *:[p]:not-first:mt-4">
+            <div class="flex flex-col md:*:text-3xl lg:*:text-2xl text-white/80 *:[p]:not-first:mt-4">
                 <?= $site->tourDesc()->kt() ?>
-            <a href="<?= $site->tourButton()->toUrl() ?>" class="inline-block font-semibold text-shadow-2xs text-shadow-black mt-8 uppercase bg-red-600 rounded-4xl px-8 py-3">Formulario de contacto</a>
+                <a href="<?= $site->tourButton()->toUrl() ?>" class="inline-block mx-auto font-semibold text-shadow-2xs text-shadow-black mt-8 md:mt-12 lg:mt-8 uppercase bg-red-600 rounded-4xl px-8 py-3">Formulario de contacto</a>
             </div>
         </div>
     </div>
