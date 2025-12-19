a28ed7ec4abf27fbe7cda4a618101ab6702f9b70
alv.dev <soy@alv.dev>
2025-12-19 22:52:50 +0100

    (gallery): add responsive design

--- magoscott/site/templates/gallery.php
+++ magoscott/site/templates/gallery.php
@@ -13,18 +13,18 @@ $sizes = "
 ?>
 
 <div x-data="{ selectedTab: 'pictures' }">
-    <section id="intro" class="relative bg-linear-to-bl from-red-600/30 to-indigo-950/30 to-50% pt-36">
-        <div class="container relative z-50 flex items-center gap-16 justify-center text-3xl">
-            <button x-on:click="selectedTab = 'pictures'" x-bind:class="selectedTab === 'pictures' ? 'bg-white text-black' : ''" class="w-full min-w-1/4 inline-block font-semibold text-shadow-2xs text-shadow-black mt-8 uppercase border-2 hover:bg-white hover:text-indigo-950 rounded-4xl px-8 py-3">Fotos</button>
+    <section id="intro" class="relative pt-16 md:pt-28 lg:pt-36 bg-linear-to-bl from-red-600/30 to-indigo-950/30 to-50%">
+        <div class="container relative flex flex-col md:flex-row items-center gap-8 lg:gap-16 justify-center text-xl md:text-2xl lg:text-3xl">
+            <button x-on:click="selectedTab = 'pictures'" x-bind:class="selectedTab === 'pictures' ? 'bg-white text-black' : ''" class="w-full min-w-1/4 inline-block font-semibold text-shadow-2xs text-shadow-black uppercase border-2 hover:bg-white hover:text-indigo-950 rounded-4xl px-8 py-3">Fotos</button>
 
-            <button x-on:click="selectedTab = 'videos'" x-bind:class="selectedTab === 'videos' ? 'bg-white text-black' : ''" class="w-full min-w-1/4 inline-block font-semibold text-shadow-2xs text-shadow-black mt-8 uppercase border-2 hover:bg-white hover:text-indigo-950 rounded-4xl px-8 py-3">Videos</button>
+            <button x-on:click="selectedTab = 'videos'" x-bind:class="selectedTab === 'videos' ? 'bg-white text-black' : ''" class="w-full min-w-1/4 inline-block font-semibold text-shadow-2xs text-shadow-black uppercase border-2 hover:bg-white hover:text-indigo-950 rounded-4xl px-8 py-3">Videos</button>
 
-            <button x-on:click="selectedTab = 'book'" x-bind:class="selectedTab === 'book' ? 'bg-white text-black' : ''" class="w-full min-w-1/4 inline-block font-semibold text-shadow-2xs text-shadow-black mt-8 uppercase border-2 hover:bg-white hover:text-indigo-950 rounded-4xl px-8 py-3">Book</button>
+            <button x-on:click="selectedTab = 'book'" x-bind:class="selectedTab === 'book' ? 'bg-white text-black' : ''" class="w-full min-w-1/4 inline-block font-semibold text-shadow-2xs text-shadow-black uppercase border-2 hover:bg-white hover:text-indigo-950 rounded-4xl px-8 py-3">Book</button>
         </div>
     </section>
 
     <!-- Videos -->
-    <div x-show="selectedTab === 'videos'" x-transition x-cloak class="container mt-36 grid grid-cols-3 gap-8">
+    <div x-show="selectedTab === 'videos'" x-transition x-cloak class="container mt-16 md:mt-28 lg:mt-36 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
         <?php foreach ($page->videoList()->toStructure() as $item): ?>
             <div class="flex gap-8 *:w-full *:h-auto *:rounded-xl *:ring-2 *:ring-indigo-900/50 *:aspect-video">
                 <?= $item->video()->toEmbed()->code() ?>
@@ -71,7 +71,7 @@ $sizes = "
         @keyup.left.window="imageGalleryPrev();"
         class="w-full h-full select-none">
 
-        <div class="container mt-36 opacity-0 duration-1000 delay-300 select-none ease animate-fade-in-view" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px);">
+        <div class="container mt-16 md:mt-28 lg:mt-36 opacity-0 duration-1000 delay-300 select-none ease animate-fade-in-view" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px);">
             <ul x-ref="gallery" id="gallery" class="mt-8 columns-2 *:break-inside-avoid items-center md:columns-3 lg:columns-4 gap-4">
                 <template x-for="(image, index) in imageGallery">
                     <li>
@@ -136,13 +136,13 @@ $sizes = "
     </div>
 
     <!-- Book -->
-    <div x-show="selectedTab === 'book'" x-transition x-cloak class="container mt-36 grid grid-cols-4 gap-8">
+    <div x-show="selectedTab === 'book'" x-transition x-cloak class="container mt-16 md:mt-28 lg:mt-36 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
         <?php foreach ($page->children()->find('book')->images() as $image): ?>
             <div class="relative flex gap-8 *:w-full *:h-auto *:rounded-xl *:ring-2 *:ring-indigo-900/50 *:aspect-2/3 *:object-cover">
                 <img src="<?= $image->url() ?>" alt="<?= $image->alt() ?>">
 
                 <div class="absolute">
-                    <a href="<?= $image->url() ?>" download="<?= $image->title() ?>" class="absolute bottom-4 left-4 right-4 flex gap-2 justify-center items-center py-4 text-2xl uppercase bg-linear-to-t from-black/60 to-violet-600/60 rounded-xl hover:bg-violet-600/60 transition-all">
+                    <a href="<?= $image->url() ?>" download="<?= $image->title() ?>" class="absolute bottom-2 left-2 right-2 md:bottom-4 md:left-4 md:right-4 flex gap-2 justify-center items-center py-2 md:py-4 text-lg md:text-xl lg:text-2xl uppercase bg-linear-to-t from-black/60 to-violet-600/60 rounded-xl hover:bg-violet-600/60 transition-all">
                         Descargar
                         <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e3e3e3">
                             <path d="m480-320 160-160-56-56-64 64v-168h-80v168l-64-64-56 56 160 160Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
