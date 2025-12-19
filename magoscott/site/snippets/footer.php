e8c771e4e25b2a7449dcd6eaf77484d64255751a
alv.dev <soy@alv.dev>
2025-12-19 21:00:40 +0100

    fix (styles): responsive md + dividers color

--- magoscott/site/snippets/footer.php
+++ magoscott/site/snippets/footer.php
@@ -1,7 +1,7 @@
 <!-- Set user var for profile links -->
 <?php $user = $kirby->users()->findBy('username', 'Scott') ?>
 
-<footer class="pt-48 bg-linear-to-b from-indigo-950 from-10% to-violet-600 to-50% text-white">
+<footer class="pt-28 md:pt-36 lg:pt-48 bg-linear-to-b from-indigo-950 from-10% to-violet-600 to-50% text-white">
     <div class="container flex flex-col md:items-center justify-center gap-8">
         <h2 class="text-4xl md:text-6xl font-semibold text-white md:whitespace-nowrap text-shadow-xs text-shadow-black">Sigue al Mago Scott</h2>
 
@@ -32,12 +32,12 @@
         </ul>
     </div>
 
-    <div class="container mt-16 pb-16 md:pb-32 flex flex-col md:flex-row justify-between gap-24 md:gap-28 md:*:w-1/2">
+    <div class="container mt-16 pb-16 md:pb-24 lg:pb-32 flex flex-col lg:flex-row justify-between gap-24 lg:gap-28 lg:*:w-1/2">
         <div class="flex flex-col">
             <h2 class="text-4xl md:text-6xl font-semibold text-white text-balance text-shadow-xs text-shadow-black">Últimos artículos</h2>
             <ul class="mt-8 grid md:grid-cols-3 gap-x-8 gap-y-16">
                 <?php foreach ($site->find('blog')->children()->listed()->limit(3) as $article): ?>
-                    <li class="group relative after:absolute after:left-0 after:right-0 after:-bottom-8 md:after:-bottom-4 after:h-0.5 md:after:h-1 after:bg-fuchsia-500 after:rounded-full group-hover:after:bg-red-700">
+                    <li class="group relative after:absolute after:left-0 after:right-0 after:-bottom-8 md:after:-bottom-4 after:h-0.5 md:after:h-1 after:bg-white/50 after:rounded-full group-hover:after:bg-red-700">
                         <a href="<?= $article->url() ?>" class="grid gap-2">
                             <div class="grid grid-cols-2 md:grid-cols-1 items-center gap-4">
                                 <div class="*:[img]:border-2 *:[img]:border-violet-500 *:[img]:rounded-xl *:[img]:bg-violet-700 *:[img]:aspect-video *:[img]:object-cover group-hover:*:[img]:rounded-3xl group-hover:*:[img]:transition-all *:transition-all">
@@ -57,7 +57,7 @@
         </div>
 
         <div>
-            <h2 class="text-4xl md:text-6xl font-semibold text-white text-balance text-shadow-xs text-shadow-black">Una newsletter mágica</h2>
+            <h2 class="text-4xl md:text-6xl font-semibold text-white  text-shadow-xs text-shadow-black">Una newsletter mágica</h2>
             <p class="mt-4 md:mt-8 text-xl md:text-3xl text-violet-100 text-shadow-black">Mantente al tanto de todos los espectáculos, sorteos y entradas gratis para asistir en directo a los shows del Mago Scott.</p>
             <form action="#" class="mt-8 md:mt-16">
                 <div class="relative w-full flex flex-col md:flex-row gap-4 items-center">
