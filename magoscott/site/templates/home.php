b26e068b19c17cdf99834bda0d9d013ccd0c7bdd
alv.dev <soy@alv.dev>
2025-12-20 18:36:14 +0100

    ⚰️remove dead code

--- magoscott/site/templates/home.php
+++ magoscott/site/templates/home.php
@@ -2,11 +2,6 @@
 
 <?php slot('default') ?>
 
-<!-- Blocks from the visual panel -->
-
-<!-- Separator -->
-
-<?= $page->text()->toBlocks() ?>
 <?php snippet('sections/headers/home') ?>
 <?php snippet('sections/intro', ['class' => 'pt-16 md:pt-28 lg:pt-36']) ?>
 <?php snippet('sections/shows', ['class' => 'mt-24 md:mt-28 lg:mt-36']) ?>
