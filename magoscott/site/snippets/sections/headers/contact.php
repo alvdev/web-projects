32024af154e1708e7686e16171d6dd3989a8c0ec
alv.dev <soy@alv.dev>
2025-12-20 01:49:44 +0100

    (contact): add responsive design

--- magoscott/site/snippets/sections/headers/contact.php
+++ magoscott/site/snippets/sections/headers/contact.php
@@ -1,11 +1,11 @@
 <header id="home-header">
-    <div class="absolute top-0 -z-10 opacity-90 *:mask-b-to-90%">
+    <div class="absolute top-0 -z-10 w-full opacity-90 *:mask-b-to-90% *:[img]:w-full *:[img]:object-cover *:[img]:aspect-video">
         <?= asset('assets/images/headers/scott-stage.webp') ?>
     </div>
 
-    <div class="container relative pt-64">
-        <hgroup class="text-center text-balance max-w-2/3 mx-auto text-shadow-sm text-shadow-black">
-            <h1 class="pb-32 text-8xl font-bold leading-none text-balance">Contacta con el Mago Scott</h1>
+    <div class="container relative pt-28 md:pt-36 lg:pt-64">
+        <hgroup class="text-center text-balance lg:max-w-2/3 mx-auto text-shadow-sm text-shadow-black">
+            <h1 class="text-5xl md:text-7xl font-bold leading-none">Contacta con el Mago Scott</h1>
         </hgroup>
     </div>
 </header>
--- magoscott/site/templates/contact.php
+++ magoscott/site/templates/contact.php
@@ -4,7 +4,7 @@
 
 <?php snippet('sections/headers/contact') ?>
 
-<section class="infinite-scroll-h py-8 **:border-0 **:gap-2 text-9xl uppercase font-extrabold text-violet-400/40">
+<section class="infinite-scroll-h pt-16 pb-26 md:pt-24 md:pb-34 lg:pt-32 lg:pb-42 **:border-0 **:gap-2 text-5xl md:text-7xl lg:text-9xl uppercase font-extrabold text-violet-400/40">
     <div class="relative overflow-x-clip">
         <div class="carousel flex items-center w-max *:whitespace-nowrap *:after:content-['/'] animate-infinite-scroll-r hover:animate-pause" style="animation-duration: 20s">
             <div class="w-full">
@@ -35,10 +35,10 @@
     </div>
 </section>
 
-<div class="container mt-48 flex items-center gap-32 *:w-1/2">
+<div class="container flex flex-col lg:flex-row items-center lg:gap-32 w-full *:w-full lg:*:w-1/2">
     <hgroup>
-        <h1 class="text-7xl font-bold leading-none text-balance">¿Hablamos?</h1>
-        <p class="mt-8 text-4xl text-pretty">Envíanos un email si necesitas información para la contratación de los espectáculos. Resolveremos todas tus dudas en el menor tiempo posible.</p>
+        <h1 class="text-5xl md:text-6xl lg:text-7xl font-bold leading-none text-balance">¿Hablamos?</h1>
+        <p class="mt-8 text-2xl md:text-3xl lg:text-4xl text-pretty">Envíanos un email si necesitas información para la contratación de los espectáculos. Resolveremos todas tus dudas en el menor tiempo posible.</p>
     </hgroup>
 
     <?php snippet('forms/contact') ?>
