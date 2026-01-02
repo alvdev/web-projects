f76d8e927de0ae5adc7659e060be2dbdfdba9956
alv.dev <soy@alv.dev>
2026-01-03 00:54:58 +0100

    feat: add responsive cover image

--- magoscott/site/snippets/sections/headers/default.php
+++ magoscott/site/snippets/sections/headers/default.php
@@ -1,6 +1,32 @@
+<?php 
+$sizes = "
+        (min-width: 1200px) 25vw,
+        (min-width: 900px) 33vw,
+        (min-width: 600px) 50vw,
+        100vw";
+?>
 <header id="home-header" class="bg-linear-to-tl from-red-600/30 via-indigo-950/30 via-50% to-black/30">
     <div class="absolute top-0 -z-10 w-full opacity-90 *:mask-b-to-90% *:[img]:w-full *:[img]:object-cover *:[img]:aspect-video">
-        <?= $page->cover()->toFile() ?>
+        <?php if ($image = $page->cover()->toFile()): ?>
+            <picture>
+                <!-- Mobile: Vertical 1:3 Crop (Strict) -->
+                <source media="(max-width: 600px)" srcset="<?= $image->srcset('avif-mobile') ?>" sizes="100vw">
+                <source media="(max-width: 600px)" srcset="<?= $image->srcset('webp-mobile') ?>" sizes="100vw">
+
+                <!-- Desktop: Landscape -->
+                <source srcset="<?= $image->srcset('avif') ?>" type="image/avif" sizes="(min-width: 1024px) 50vw, 100vw">
+                <source srcset="<?= $image->srcset('webp') ?>" type="image/webp" sizes="(min-width: 1024px) 50vw, 100vw">
+                <img
+                    srcset="<?= $image->srcset('default') ?>"
+                    sizes="<?= $sizes ?>"
+                    src="<?= $image->resize(600)->url() ?>"
+                    width="<?= $image->width() ?>"
+                    height="<?= $image->height() ?>"
+                    alt="Scott - Magoscott"
+                    fetchpriority="high"
+                    class="w-full h-full object-cover">
+            </picture>
+        <?php endif ?>
     </div>
 
     <div class="container relative pt-28 md:pt-36 lg:pt-64">
