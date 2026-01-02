bcb8e5ca4075e0fe3724075a632a449dc703c281
alv.dev <soy@alv.dev>
2026-01-02 20:24:05 +0100

    ⚡️perf: add quality + sharpen to srcsets

--- magoscott/site/config/config.php
+++ magoscott/site/config/config.php
@@ -147,18 +147,18 @@ return [
                         '1800w' => ['width' => 1800]
                     ],
                     'avif' => [
-                        '300w'  => ['width' => 300, 'format' => 'avif'],
-                        '600w'  => ['width' => 600, 'format' => 'avif'],
-                        '900w'  => ['width' => 900, 'format' => 'avif'],
-                        '1200w' => ['width' => 1200, 'format' => 'avif'],
-                        '1800w' => ['width' => 1800, 'format' => 'avif']
+                        '300w'  => ['width' => 300, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+                        '600w'  => ['width' => 600, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+                        '900w'  => ['width' => 900, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+                        '1200w' => ['width' => 1200, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+                        '1800w' => ['width' => 1800, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35]
                     ],
                     'webp' => [
-                        '300w'  => ['width' => 300, 'format' => 'webp'],
-                        '600w'  => ['width' => 600, 'format' => 'webp'],
-                        '900w'  => ['width' => 900, 'format' => 'webp'],
-                        '1200w' => ['width' => 1200, 'format' => 'webp'],
-                        '1800w' => ['width' => 1800, 'format' => 'webp']
+                        '300w'  => ['width' => 300, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+                        '600w'  => ['width' => 600, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+                        '900w'  => ['width' => 900, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+                        '1200w' => ['width' => 1200, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+                        '1800w' => ['width' => 1800, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35]
                     ],
                 ]
             ],
