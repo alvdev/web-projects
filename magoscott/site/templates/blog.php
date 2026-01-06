30a89e5221f1c68a2422dfd8dc3dcf772c87dfa3
alv.dev <soy@alv.dev>
2026-01-06 19:20:01 +0100

    add placeholder cover images to posts

--- magoscott/site/templates/blog.php
+++ magoscott/site/templates/blog.php
@@ -9,6 +9,30 @@ $sizes = "
         (min-width: 900px) 33vw,
         (min-width: 600px) 50vw,
         100vw";
+
+$srcset169Avif = [
+    '300w'  => ['width' => 300, 'height' => 169, 'crop' => true, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+    '600w'  => ['width' => 600, 'height' => 338, 'crop' => true, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+    '900w'  => ['width' => 900, 'height' => 506, 'crop' => true, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+    '1200w' => ['width' => 1200, 'height' => 675, 'crop' => true, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35],
+    '1800w' => ['width' => 1800, 'height' => 1013, 'crop' => true, 'format' => 'avif', 'quality' => 70, 'sharpen' => 35]
+];
+
+$srcset169Webp = [
+    '300w'  => ['width' => 300, 'height' => 169, 'crop' => true, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+    '600w'  => ['width' => 600, 'height' => 338, 'crop' => true, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+    '900w'  => ['width' => 900, 'height' => 506, 'crop' => true, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+    '1200w' => ['width' => 1200, 'height' => 675, 'crop' => true, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35],
+    '1800w' => ['width' => 1800, 'height' => 1013, 'crop' => true, 'format' => 'webp', 'quality' => 80, 'sharpen' => 35]
+];
+
+$srcset169Default = [
+    '300w'  => ['width' => 300, 'height' => 169, 'crop' => true],
+    '600w'  => ['width' => 600, 'height' => 338, 'crop' => true],
+    '900w'  => ['width' => 900, 'height' => 506, 'crop' => true],
+    '1200w' => ['width' => 1200, 'height' => 675, 'crop' => true],
+    '1800w' => ['width' => 1800, 'height' => 1013, 'crop' => true]
+];
 ?>
 
 <main class="pt-16 md:pt-28 lg:pt-36 bg-linear-to-bl from-red-600/30 via-indigo-950/30">
@@ -16,23 +40,44 @@ $sizes = "
         <?php foreach ($page->children()->listed() as $post): ?>
             <article class="grid">
                 <a href="<?= $post->url() ?>" class="group">
-                    <picture class="*:[img]:object-cover *:[img]:aspect-video *:[img]:rounded-2xl *:[img]:group-hover:rounded-[4rem] *:[img]:transition-all *:[img]:border-2 *:[img]:border-indigo-900/50 *:[img]:bg-violet-950">
-                        <source
-                            srcset="<?= $post->cover()->toFile()->srcset('avif') ?>"
-                            sizes="<?= $sizes ?>"
-                            type="image/avif">
-                        <source
-                            srcset="<?= $post->cover()->toFile()->srcset('webp') ?>"
-                            sizes="<?= $sizes ?>"
-                            type="image/webp">
-                        <img
-                            alt="<?= $post->cover()->toFile()->alt() ?>"
-                            src="<?= $post->cover()->toFile()->resize(300)->url() ?>"
-                            srcset="<?= $post->cover()->toFile()->srcset() ?>"
-                            sizes="<?= $sizes ?>"
-                            width="<?= $post->cover()->toFile()->resize(1800)->width() ?>"
-                            height="<?= $post->cover()->toFile()->resize(1800)->height() ?>">
-                    </picture>
+                    <?php if ($post->cover()->isNotEmpty()): ?>
+                        <picture class="*:[img]:object-cover *:[img]:aspect-video *:[img]:rounded-2xl *:[img]:group-hover:rounded-[4rem] *:[img]:transition-all *:[img]:border-2 *:[img]:border-indigo-900/50 *:[img]:bg-violet-950">
+                            <source
+                                srcset="<?= $post->cover()->toFile()->srcset($srcset169Avif) ?>"
+                                sizes="<?= $sizes ?>"
+                                type="image/avif">
+                            <source
+                                srcset="<?= $post->cover()->toFile()->srcset($srcset169Webp) ?>"
+                                sizes="<?= $sizes ?>"
+                                type="image/webp">
+                            <img
+                                alt="<?= $post->cover()->toFile()->alt() ?>"
+                                src="<?= $post->cover()->toFile()->crop(600, 338)->url() ?>"
+                                srcset="<?= $post->cover()->toFile()->srcset($srcset169Default) ?>"
+                                sizes="<?= $sizes ?>"
+                                width="1800"
+                                height="1013">
+                        </picture>
+                    <?php else: ?>
+                        <?php $defaultCover = asset('/assets/images/headers/post-placeholder.webp'); ?>
+                        <picture class="*:[img]:object-cover *:[img]:aspect-video *:[img]:rounded-2xl *:[img]:group-hover:rounded-[4rem] *:[img]:transition-all *:[img]:border-2 *:[img]:border-indigo-900/50 *:[img]:bg-violet-950">
+                            <source
+                                srcset="<?= $defaultCover->srcset($srcset169Avif) ?>"
+                                sizes="<?= $sizes ?>"
+                                type="image/avif">
+                            <source
+                                srcset="<?= $defaultCover->srcset($srcset169Webp) ?>"
+                                sizes="<?= $sizes ?>"
+                                type="image/webp">
+                            <img
+                                alt="Placeholder image for <?= $post->title() ?>"
+                                src="<?= $defaultCover->crop(600, 338)->url() ?>"
+                                srcset="<?= $defaultCover->srcset($srcset169Default) ?>"
+                                sizes="<?= $sizes ?>"
+                                width="1800"
+                                height="1013">
+                        </picture>
+                    <?php endif ?>
 
                     <h2 class="mt-8 relative pl-12 text-3xl group-hover:text-violet-300 before:absolute before:left-0 before:top-1 before:w-8 before:h-8 before:bg-red-500 before:rounded-full">
                         <?= $post->title() ?>
