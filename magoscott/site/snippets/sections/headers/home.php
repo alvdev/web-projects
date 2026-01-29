fbf98f1c7a78fad77d3cdfc1313505b44d61ef04
alv.dev <soy@alv.dev>
2026-01-29 18:24:00 +0100

    style: add reverse home header + style improvements

--- magoscott/site/snippets/sections/headers/home.php
+++ magoscott/site/snippets/sections/headers/home.php
@@ -1,5 +1,5 @@
 <section id="home-header" class="bg-linear-to-tl from-red-600 via-indigo-950 via-50% to-black">
-    <div class="container pt-28 md:pt-36 lg:pt-64 flex flex-col lg:flex-row items-center gap-8 md:gap-16 lg:gap-32 lg:*:w-1/2">
+    <div class="container pt-24 md:pt-36 lg:pt-64 flex flex-col-reverse lg:flex-row items-center gap-8 md:gap-16 lg:gap-32 lg:*:w-1/2">
         <div>
             <hgroup class="*:[h1]:text-4xl md:*:[h1]:text-7xl *:[h1]:font-bold *:[h1]:leading-none *:[h1]:text-balance *:[p]:mt-8 *:[p]:text-xl md:*:[p]:text-4xl *:[p]:text-pretty *:[p]:text-white/80">
                 <?= $site->formText()->kt() ?>
