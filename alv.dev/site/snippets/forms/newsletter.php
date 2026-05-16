1e6896f4c02b6f7f3e902951db508e6b894c14fd
alv.dev <soy@alv.dev>
2026-05-16 19:11:17 +0200

    style: improve form styles

--- alv.dev/site/snippets/forms/newsletter.php
+++ alv.dev/site/snippets/forms/newsletter.php
@@ -1,10 +1,19 @@
 <?php
 
+$classes = $classes ?? '';
+$isDark = $isDark ?? false;
+
+if ($isDark) {
+    $classes .= '**:[input]:border-green-500 **:[button]:bg-green-500 **:[button]:text-black';
+} else {
+    $classes .= '**:[input]:border-black **:[button]:bg-black **:[button]:text-green-500';
+}
+
 snippet('dreamform/form', [
     'form' => $site->find('forms/newsletter'),
     'attr' => [
         'form' => [
-            'class' => 'flex flex-col items-center',
+            'class' => 'flex flex-col items-center ' . $classes,
             'x-init',
             'x-target' => 'newsletter-message'
         ],
@@ -12,7 +21,7 @@ snippet('dreamform/form', [
         'field' => ['class' => 'group flex flex-col items-start'],
         'label' => ['class' => 'hidden text-sm font-medium text-slate-600 mb-1'],
         'input' => ['class' => 'w-full bg-transparent border-b-2 border-green-500 text-green-500 placeholder:text-green-100 placeholder:font-light pb-2 outline-hidden peer group-data-has-error:placeholder:text-white group-data-has-error:mb-2 shadow-xs placeholder-shown:group-data-has-error:border-rose-400'],
-        'button' => ['class' => 'bg-green-500 rounded-xs text-black inline-block w-fit min-w-[50%] px-8 py-2 mt-4 hover:animate-wiggle'],
+        'button' => ['class' => 'rounded-xs inline-block w-fit min-w-[50%] px-8 py-2 mt-4 hover:animate-wiggle'],
         'error' => ['class' => 'peer-not-placeholder-shown:hidden group-data-has-error:-mb-4 text-rose-400 text-sm'],
         'radio' => [
             'label' => ['class' => 'text-sm font-medium text-slate-600 mb-2'],
@@ -28,6 +37,6 @@ snippet('dreamform/form', [
         ],
         'success' => [
             'class' => 'text-green-500 border-2 border-green-500 px-4 py-8 text-center text-sm'
-            ]
+        ]
     ]
 ]);
