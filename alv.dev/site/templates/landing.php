8cfb4922ea1f3b5f192e05a2fac197eac026d51f
alv.dev <soy@alv.dev>
2026-05-16 22:20:41 +0200

    add landing template

---
+++ alv.dev/site/templates/landing.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="<?= t('lang', 'es') ?>">
+
+<?php snippet('head') ?>
+
+<body class="debug-screens flex flex-col h-full font-mono">
+    <div class="flex">
+        <?php snippet('navbar') ?>
+
+        <div class="w-full">
+            <main class="flex-1">
+                <?php snippet('header') ?>
+
+                <div class="container mt-24">
+                    <div class="relative -z-50 border-6 border-gray-950 rounded-xs">
+                    </div>
+                </div>
+
+                <div class="flex-1 min-h-screen">
+                    <?php snippet('sections/skills-grid') ?>
+                    <?php snippet('sections/projects') ?>
+                    <?php snippet('sections/contact-form') ?>
+                    <?php snippet('sections/about-me') ?>
+                    <?php snippet('sections/latest-posts') ?>
+                </div>
+            </main>
+
+            <?php snippet('footer') ?>
+        </div>
+    </div>
+</body>
+
+</html>
