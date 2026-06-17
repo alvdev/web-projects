bd92edd87a4e5fa863c478a69420586ab4190cfb
alv.dev <soy@alv.dev>
2026-06-17 05:13:33 +0200

    add services template

---
+++ alv.dev/site/templates/services.php
@@ -0,0 +1,19 @@
+<?php snippet('base', slots: true) ?>
+<?php slot('hero') ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Services</h1>
+        </div>
+    </div>
+</div>
+<?php endslot() ?>
+<?php slot('content') ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <p>Services</p>
+        </div>
+    </div>
+</div>
+<?php endslot() ?>
