97c352be3430a17ef4028137846dff159ae20e58
alv.dev <soy@alv.dev>
2026-06-19 06:24:31 +0200

    remove & untrack prod folders

--- diario.games/vendor/loupe/loupe/src/Internal/Cache/InvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Loupe\Loupe\Internal\Cache;
-
-final class InvalidArgumentException extends \InvalidArgumentException implements \Psr\Cache\InvalidArgumentException
-{
-}
