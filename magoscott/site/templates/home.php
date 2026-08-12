1aac60834c0ef4aa07ba8e0a1fea10c7799dbc50
alv.dev <soy@alv.dev>
2026-08-12 05:56:40 +0200

    fix: add visible managed Turnstile captcha to avoid false negatives

--- magoscott/site/templates/home.php
+++ magoscott/site/templates/home.php
@@ -3,7 +3,7 @@
 <?php slot('default') ?>
 
 <?php snippet('sections/headers/home') ?>
-<?php snippet('sections/intro', ['class' => 'pt-16 md:pt-28 lg:pt-36']) ?>
+<?php snippet('sections/intro', ['class' => 'pt-16 pt-44 md:pt-48 lg:pt-56']) ?>
 <?php snippet('sections/shows', ['class' => 'mt-24 md:mt-28 lg:mt-36']) ?>
 <?php snippet('sections/tours', ['class' => 'mt-24 md:mt-28 lg:mt-36']) ?>
 
